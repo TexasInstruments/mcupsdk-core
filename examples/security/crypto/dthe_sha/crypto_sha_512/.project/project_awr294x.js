da295e712a1d8ed3a2a7b3bdabdab73639cc97ff
Ashish Singh <a-singh3@ti.com>
2023-05-22 21:48:57 +0530

    awr294x: tifs: port dthe SHA examples for R5 to awr294x
    -Port sha and hmac_sha examples to awr294x
    -Add the syscfg file for each example
    -Add and update main.c, .js files, make files, linker for build support
    
    Fixes: MMWSDK-2714, MMWSDK-2716
    
    Signed-off-by: Ashish Singh <a-singh3@ti.com>

---
+++ examples/security/crypto/dthe_sha/crypto_sha_512/.project/project_awr294x.js
@@ -0,0 +1,173 @@
+let path = require('path');
+
+let device = "awr294x";
+
+const files = {
+    common: [
+        "crypto_sha_512.c",
+        "main.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",       /* core_os_combo base */
+        "../../..", /* Example base */
+    ],
+};
+
+const libdirs_nortos = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+        "${MCU_PLUS_SDK_PATH}/source/security/lib",
+    ],
+};
+
+const libdirs_freertos = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+    ],
+};
+
+const includes_freertos_r5f = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_ARM_CLANG/ARM_CR5F",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/awr294x/r5f",
+    ],
+};
+
+const libs_nortos_r5f = {
+    common: [
+        "nortos.awr294x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.awr294x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.awr294x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "security.awr294x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_r5f = {
+    common: [
+        "freertos.awr294x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.awr294x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.awr294x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const includes_freertos_c66 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_CGT/DSP_C66",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/awr294x/c66",
+    ],
+};
+
+const libs_nortos_c66 = {
+    common: [
+        "nortos.awr294x.c66.ti-c6000.${ConfigName}.lib",
+        "drivers.awr294x.c66.ti-c6000.${ConfigName}.lib",
+        "board.awr294x.c66.ti-c6000.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_c66 = {
+    common: [
+        "freertos.awr294x.c66.ti-c6000.${ConfigName}.lib",
+        "drivers.awr294x.c66.ti-c6000.${ConfigName}.lib",
+        "board.awr294x.c66.ti-c6000.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const syscfgfile = "../example.syscfg";
+
+const templates_nortos_r5f =
+[
+    {
+        input: ".project/templates/awr294x/common/linker_r5f.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/awr294x/nortos/main_nortos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "crypto_sha_512_main",
+        },
+    }
+];
+
+const templates_freertos_r5f =
+[
+    {
+        input: ".project/templates/awr294x/common/linker_r5f.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/awr294x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "crypto_sha_512_main",
+        },
+    }
+];
+
+
+const buildOptionCombos = [
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "awr294x-evm", os: "nortos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "crypto_sha_512";
+    property.isInternal = false;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.libdirs = libdirs_nortos;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+
+    if(buildOption.cpu.match(/r5f*/)) {
+        if(buildOption.os.match(/freertos*/) )
+        {
+            build_property.includes = includes_freertos_r5f;
+            build_property.libdirs = libdirs_freertos;
+            build_property.libs = libs_freertos_r5f;
+            build_property.templates = templates_freertos_r5f;
+        }
+        else
+        {
+            build_property.libs = libs_nortos_r5f;
+            build_property.templates = templates_nortos_r5f;
+        }
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
