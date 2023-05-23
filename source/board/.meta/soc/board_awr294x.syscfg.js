12005533b298b13772c9acdf4db749d155345f93
Ashish Singh <a-singh3@ti.com>
2023-05-23 15:32:19 +0530

    awr294x: tifs: update syscfg to remove board drivers for hsm core
    -hsm core doesn't require board driver in syscfg
    -update board_awr294x_syscfg.js to remove ti_board_driver for hsm core
    
    Fixes: MMWSDK-2933
    
    Signed-off-by: Ashish Singh <a-singh3@ti.com>

--- source/board/.meta/soc/board_awr294x.syscfg.js
+++ source/board/.meta/soc/board_awr294x.syscfg.js
@@ -15,11 +15,17 @@ const topModules_r5f = [
 const topModules_c66 = [
 ];
 
+const topModules_hsm = [
+];
+
 exports = {
     getTopModules: function() {
         let topModules = topModules_r5f;
         if(common.getSelfSysCfgCoreName().includes("c66")) {
             topModules = topModules_c66;
+        }
+        else if(common.getSelfSysCfgCoreName().includes("hsm")) {
+            topModules = topModules_hsm;
         }
         return topModules;
     },
