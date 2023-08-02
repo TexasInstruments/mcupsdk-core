68b7d2a8312797c5c9ed9b113e7964a3e9956b52
Aswathi R <a0505177@ti.com>
2023-08-02 10:39:09 +0530

    allSOC: pmu: Fix post-build steps fail
    - Fix project name (remove extra space) in all project_{soc}.js files
    
    Fixes: MCUSDK-11363
    Signed-off-by: Aswathi R <a-r1@ti.com>

--- examples/drivers/pmu/pmu_multievent/.project/project_am243x.js
+++ examples/drivers/pmu/pmu_multievent/.project/project_am243x.js
@@ -74,7 +74,7 @@ function getComponentProperty() {
 
     property.dirPath = path.resolve(__dirname, "..");
     property.type = "executable";
-    property.name = " pmu_multievent";
+    property.name = "pmu_multievent";
     property.isInternal = false;
     property.buildOptionCombos = buildOptionCombos;
 
