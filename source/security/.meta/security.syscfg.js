08d6216d5ecc60c6e82b0e02025366a88bd70ca7
Utkarsh M Kripashankar <u-kripashankar@ti.com>
2023-11-08 11:28:05 +0530

    am263x/am273x/am263px: syscfg: hsm services syscfg bug-fix
    hsm services module visibility should only be from public cores
    and not from hsm core as these services are meant for public cores
    to request to hsm.
    
    Fixes: TIFSMCU-1158
    
    Signed-off-by: Utkarsh M Kripashankar <u-kripashankar@ti.com>

--- source/security/.meta/security.syscfg.js
+++ source/security/.meta/security.syscfg.js
@@ -2,7 +2,15 @@
 let common = system.getScript("/common");
 let soc = system.getScript(`/security/soc/security_${common.getSocName()}`);
 
-exports = {
-    displayName: "TI HSM Services",
-    topModules: soc.getTopModules(),
-};
+let selfCoreName = common.getSelfSysCfgCoreName();
+
+let SecServicesObj = {}
+
+if (!selfCoreName.includes("hsm")) {
+    SecServicesObj = {
+        displayName: "TI HSM Services",
+        topModules: soc.getTopModules(),
+    };
+
+}
+exports = SecServicesObj;
