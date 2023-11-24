62af28f822ad9ab223ad3e0e7d5dd6767d71c3e1
Susheel <v-voora1@ti.com>
2023-11-24 10:03:05 +0530

    am64x: ENET: ICSSG: Build fix
    Fix the path for FreeRTOSConfig in makefile for ICSSG example.
    
    Fixes: MCUSDK-10397
    
    Signed-off-by: Susheel <v-voora1@ti.com>

--- examples/networking/tsn/gptp_icssg_app/.project/project_am64x.js
+++ examples/networking/tsn/gptp_icssg_app/.project/project_am64x.js
@@ -42,7 +42,7 @@ const includes_freertos_r5f = {
     common: [
         "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
         "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_ARM_CLANG/ARM_CR5F",
-        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am243x/r5f",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am64x/r5f",
         "${MCU_PLUS_SDK_PATH}/source/networking/enet",
         "${MCU_PLUS_SDK_PATH}/source/networking/enet/utils",
         "${MCU_PLUS_SDK_PATH}/source/networking/enet/utils/include",
