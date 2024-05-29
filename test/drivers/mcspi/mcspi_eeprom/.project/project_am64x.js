aa8be8720cf30d16b685191ada7a9b7ce1485a86
Rohith R <rohith.rajan@ti.com>
2024-05-29 09:47:48 +0530

    am64x: a53: mcspi: mcspi_eeprom
        * Add build support for the mcspi_eeprom unit test on a53
          running freertos in am64x
    
    Fixes: SITSW-4570
    
    Signed-off-by: Rohith R <rohith.rajan@ti.com>

--- test/drivers/mcspi/mcspi_eeprom/.project/project_am64x.js
+++ test/drivers/mcspi/mcspi_eeprom/.project/project_am64x.js
@@ -25,10 +25,35 @@ const libdirs = {
     ],
 };
 
+const libdirs_freertos_a53 = {
+	common: [
+		"${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+		"${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/test/unity/lib",
+	],
+};
+
+const includes_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/GCC/ARM_CA53",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am64x/a53",
+        "${MCU_PLUS_SDK_PATH}/test/unity/",
+    ],
+};
+
 const libs = {
     common: [
         "nortos.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
         "drivers.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_a53 = {
+    common: [
+        "freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "unity.am64x.a53.gcc-aarch64.${ConfigName}.lib",
     ],
 };
 
@@ -38,6 +63,11 @@ const lnkfiles = {
     ]
 };
 
+const cflags_a53 = {
+    common: [
+        "-Wno-unused-function"
+    ]
+}
 const syscfgfile = "../example.syscfg"
 
 const readmeDoxygenPageTag = "DRIVERS_MCSPI_PAGE";
@@ -53,8 +83,24 @@ const templates_nortos_r5f =
     }
 ];
 
+const templates_freertos_a53 =
+[
+    {
+        input: ".project/templates/am64x/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am64x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "mcspi_eeprom_main",
+        },
+    },
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-evm", os: "nortos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am64x-evm", os: "freertos"},
 ];
 
 function getComponentProperty() {
@@ -75,12 +121,22 @@ function getComponentBuildProperty(buildOption) {
 
     build_property.files = files;
     build_property.filedirs = filedirs;
-    build_property.libdirs = libdirs;
     build_property.lnkfiles = lnkfiles;
-    build_property.libs = libs;
     build_property.syscfgfile = syscfgfile;
     build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
-    build_property.templates = templates_nortos_r5f;
+
+    if(buildOption.cpu.match(/r5f*/)) {
+        build_property.libs = libs;
+        build_property.templates = templates_nortos_r5f;
+        build_property.libdirs = libdirs;
+    }
+    if(buildOption.cpu.match(/a53*/)) {
+        build_property.includes = includes_freertos_a53;
+        build_property.libs = libs_freertos_a53;
+        build_property.templates = templates_freertos_a53;
+        build_property.libdirs = libdirs_freertos_a53;
+        build_property.cflags = cflags_a53;
+    }
 
     return build_property;
 }
