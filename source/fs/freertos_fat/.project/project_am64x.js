b6316a26dda3767b626ff3915ba010894a3db718
Rohith R <rohith.rajan@ti.com>
2024-06-06 14:01:49 +0530

    am64x: a53: FreeRTOS-FAT: library and sysconfig support
        * Add FreeRTOS FAT support on a53 in am64x-evm
        * Add sysconfig support for FreeRTOS FAT on a53 in am64x-evm
    
    Fixes: SITSW-4654
    
    Signed-off-by: Rohith R <rohith.rajan@ti.com>

--- source/fs/freertos_fat/.project/project_am64x.js
+++ source/fs/freertos_fat/.project/project_am64x.js
@@ -37,17 +37,36 @@ const includes_r5f = {
     ],
 };
 
+const includes_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/FreeRTOS-FAT/include",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/config",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable/nortos",
+    ],
+};
 
 const cflags = {
     common: [
         "-Wno-extra",
         "-Wno-uninitialized",
-        "-Wno-unused-but-set-variable",        
+        "-Wno-unused-but-set-variable",
+
+    ],
+};
+const cflags_a53 = {
+    common: [
+        "-Wno-extra",
+        "-Wno-uninitialized",
+        "-Wno-unused-but-set-variable",
+        "-Wno-stringop-truncation",
+        "-Wno-overflow"
     ],
 };
 
 const buildOptionCombos = [
     { device: device, cpu: "r5f", cgt: "ti-arm-clang"},
+    { device: device, cpu: "a53", cgt: "gcc-aarch64"},
 ];
 
 function getComponentProperty() {
@@ -67,9 +86,13 @@ function getComponentBuildProperty(buildOption) {
 
     build_property.filedirs = filedirs;
     build_property.files = files;
-    build_property.cflags = cflags;
     if(buildOption.cpu.match(/r5f*/)) {
         build_property.includes = includes_r5f;
+        build_property.cflags = cflags;
+    }
+    if(buildOption.cpu.match(/a53*/)) {
+        build_property.includes = includes_a53;
+        build_property.cflags = cflags_a53;
     }
 
     return build_property;
