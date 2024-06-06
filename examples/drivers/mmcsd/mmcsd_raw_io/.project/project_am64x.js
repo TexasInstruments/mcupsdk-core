cdf8ad7060fd01ffe315db325113f6b4af9457a7
Rohith R <rohith.rajan@ti.com>
2024-06-06 13:51:07 +0530

    am64x: a53: mmcsd: mmcsd_raw_io
        * Add build support for the mmcsd_raw_io example on a53 running
          FreeRTOS in am64x-evm
        * Update the documentation for this example
    
    Fixes: SITSW-4654
    
    Signed-off-by: Rohith R <rohith.rajan@ti.com>

--- examples/drivers/mmcsd/mmcsd_raw_io/.project/project_am64x.js
+++ examples/drivers/mmcsd/mmcsd_raw_io/.project/project_am64x.js
@@ -26,10 +26,32 @@ const libdirs = {
     ],
 };
 
+const libdirs_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+    ],
+};
+
+const libs_freertos_a53 = {
+    common: [
+        "freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+    ],
+};
+
 const libs_r5f = {
     common: [
         "nortos.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
         "drivers.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const includes_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/GCC/ARM_CA53",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am64x/a53",
     ],
 };
 
@@ -54,9 +76,26 @@ const templates_nortos_r5f =
     }
 ];
 
+
+const templates_freertos_a53 =
+[
+    {
+        input: ".project/templates/am64x/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am64x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "mmcsd_raw_io_main",
+        },
+    }
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-evm", os: "nortos"},
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-sk", os: "nortos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am64x-evm", os: "freertos"},
 ];
 
 function getComponentProperty() {
@@ -84,6 +123,12 @@ function getComponentBuildProperty(buildOption) {
     if(buildOption.cpu.match(/r5f*/)) {
         build_property.libs = libs_r5f;
         build_property.templates = templates_nortos_r5f;
+    }
+    else if(buildOption.cpu.match(/a53*/)){
+        build_property.includes = includes_freertos_a53;
+        build_property.libdirs = libdirs_freertos_a53;
+        build_property.libs = libs_freertos_a53;
+        build_property.templates = templates_freertos_a53;
     }
 
     return build_property;
