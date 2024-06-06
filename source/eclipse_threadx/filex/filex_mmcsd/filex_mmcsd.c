76935174f83bc97df8fb99d35f76e43099fa7613
Edouard Haas <edouard.haas@jblopen.com>
2024-06-06 17:48:21 -0400

    am243x: threadx: Add ThreadX and FileX to the build
    - Added threadx/filex to build.
    - Added mmcsd driver glue code for filex.
    
    Fixes: SITSW-5614
    
    Signed-off-by: Edouard Haas <edouard.haas@jblopen.com>

---
+++ source/eclipse_threadx/filex/filex_mmcsd/filex_mmcsd.c
@@ -0,0 +1,244 @@
+/*
+ *  Copyright (C) 2018-2023 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <fx_api.h>
+#include <fx_port.h>
+#include <fx_media.h>
+#include <drivers/mmcsd/v0/mmcsd.h>
+
+
+VOID filex_mmcsd_driver(FX_MEDIA *media_ptr)
+{
+    MMCSD_Handle mmcsd_hndl;
+    uint32_t block_size;
+
+    /* There are several useful/important pieces of information contained in the media
+       structure, some of which are supplied by FileX and others are for the driver to
+       setup. The following is a summary of the necessary FX_MEDIA structure members:
+
+            FX_MEDIA Member                              Meaning
+
+        fx_media_driver_request             FileX request type. Valid requests from FileX are
+                                            as follows:
+
+                                                    FX_DRIVER_READ
+                                                    FX_DRIVER_WRITE
+                                                    FX_DRIVER_FLUSH
+                                                    FX_DRIVER_ABORT
+                                                    FX_DRIVER_INIT
+                                                    FX_DRIVER_BOOT_READ
+                                                    FX_DRIVER_RELEASE_SECTORS
+                                                    FX_DRIVER_BOOT_WRITE
+                                                    FX_DRIVER_UNINIT
+
+        fx_media_driver_status              This value is RETURNED by the driver. If the
+                                            operation is successful, this field should be
+                                            set to FX_SUCCESS for before returning. Otherwise,
+                                            if an error occurred, this field should be set
+                                            to FX_IO_ERROR.
+
+        fx_media_driver_buffer              Pointer to buffer to read or write sector data.
+                                            This is supplied by FileX.
+
+        fx_media_driver_logical_sector      Logical sector FileX is requesting.
+
+        fx_media_driver_sectors             Number of sectors FileX is requesting.
+
+
+       The following is a summary of the optional FX_MEDIA structure members:
+
+            FX_MEDIA Member                              Meaning
+
+        fx_media_driver_info                Pointer to any additional information or memory.
+                                            This is optional for the driver use and is setup
+                                            from the fx_media_open call. The RAM disk uses
+                                            this pointer for the RAM disk memory itself.
+
+        fx_media_driver_write_protect       The DRIVER sets this to FX_TRUE when media is write
+                                            protected. This is typically done in initialization,
+                                            but can be done anytime.
+
+        fx_media_driver_free_sector_update  The DRIVER sets this to FX_TRUE when it needs to
+                                            know when clusters are released. This is important
+                                            for FLASH wear-leveling drivers.
+
+        fx_media_driver_system_write        FileX sets this flag to FX_TRUE if the sector being
+                                            written is a system sector, e.g., a boot, FAT, or
+                                            directory sector. The driver may choose to use this
+                                            to initiate error recovery logic for greater fault
+                                            tolerance.
+
+        fx_media_driver_data_sector_read    FileX sets this flag to FX_TRUE if the sector(s) being
+                                            read are file data sectors, i.e., NOT system sectors.
+
+        fx_media_driver_sector_type         FileX sets this variable to the specific type of
+                                            sector being read or written. The following sector
+                                            types are identified:
+
+                                                    FX_UNKNOWN_SECTOR
+                                                    FX_BOOT_SECTOR
+                                                    FX_FAT_SECTOR
+                                                    FX_DIRECTORY_SECTOR
+                                                    FX_DATA_SECTOR
+    */
+
+
+    mmcsd_hndl = *((MMCSD_Handle *)media_ptr->fx_media_driver_info);
+    block_size = MMCSD_getBlockSize(mmcsd_hndl);
+
+    /* Process the driver request specified in the media control block.  */
+    switch(media_ptr->fx_media_driver_request)
+    {
+
+        case FX_DRIVER_READ:
+        {
+            uint32_t start_blk;
+            uint32_t blk_cnt;
+            int32_t res;
+
+
+            start_blk = media_ptr->fx_media_driver_logical_sector * media_ptr->fx_media_bytes_per_sector / block_size;
+            blk_cnt = media_ptr->fx_media_driver_sectors * media_ptr->fx_media_bytes_per_sector / block_size;
+
+            res = MMCSD_read(mmcsd_hndl, media_ptr->fx_media_driver_buffer, start_blk, blk_cnt);
+            if (res != SystemP_SUCCESS) {
+                media_ptr->fx_media_driver_status = FX_IO_ERROR;
+                break;
+            }
+
+            /* Successful driver request.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_WRITE:
+        {
+            uint32_t start_blk;
+            uint32_t blk_cnt;
+            int32_t res;
+
+
+            start_blk = media_ptr->fx_media_driver_logical_sector * media_ptr->fx_media_bytes_per_sector / block_size;
+            blk_cnt = media_ptr->fx_media_driver_sectors * media_ptr->fx_media_bytes_per_sector / block_size;
+
+            res = MMCSD_write(mmcsd_hndl, media_ptr->fx_media_driver_buffer, start_blk, blk_cnt);
+            if (res != SystemP_SUCCESS) {
+                media_ptr->fx_media_driver_status = FX_IO_ERROR;
+                break;
+            }
+
+            /* Successful driver request.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_RELEASE_SECTORS:
+        {
+            /* Not supported. */
+            /* Successful driver request.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_FLUSH:
+        {
+            /* Not supported. */
+            /* Return driver success.  */
+            media_ptr->fx_media_driver_status =  FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_ABORT:
+        {
+            /* Return driver success.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_INIT:
+        {
+            /* Return driver success.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_UNINIT:
+        {
+            /* Return driver success.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_BOOT_READ:
+        {
+            int res;
+
+            res = MMCSD_read(mmcsd_hndl, media_ptr->fx_media_driver_buffer, 0u, 1u);
+            if (res != SystemP_SUCCESS) {
+                media_ptr->fx_media_driver_status = FX_IO_ERROR;
+                break;
+            }
+
+
+            /* Successful driver request.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        case FX_DRIVER_BOOT_WRITE:
+        {
+            int res;
+
+            res = MMCSD_write(mmcsd_hndl, media_ptr->fx_media_driver_buffer, 0u, 1u);
+            if (res != SystemP_SUCCESS) {
+                media_ptr->fx_media_driver_status = FX_IO_ERROR;
+                break;
+            }
+
+            /* Successful driver request.  */
+            media_ptr->fx_media_driver_status = FX_SUCCESS;
+            break;
+        }
+
+        default:
+        {
+            /* Invalid driver request.  */
+            media_ptr->fx_media_driver_status =  FX_INVALID_STATE;
+            break;
+        }
+    }
+}
+
+
+
+
---
+++ source/eclipse_threadx/threadx/.project/project_am243x.js
@@ -0,0 +1,324 @@
+let path = require('path');
+
+let device = "am243x";
+
+const files = {
+    common: [
+        // picked from nortos DPL
+        "boot_armv7r.c",
+        "DebugP_shmLogWriter.c",
+        "DebugP_shmLogReader.c",
+        "DebugP_uartLogWriter.c",
+        "DebugP_uartScanf.c",
+        "DebugP_memTraceLogWriter.c",
+        "DebugP_log.c",
+        "TimerP.c",
+        "HwiP_armv7r_vim.c",
+        "MpuP_armv7r.c",
+        "CacheP_armv7r.c",
+        "CpuId_armv7r.c",
+        "HeapP_internal.c",
+        "printf.c",
+        "AddrTranslateP.c",
+        "PmuP_armv7r.c",
+        "QueueP_nortos.c",
+        // DPL common
+        "ClockP_threadx.c",
+        "DebugP_shmLogReader_threadx.c",
+        "DebugP_threadx.c",
+        "EventP_threadx.c",
+        "HeapP_threadx.c",
+        "SemaphoreP_threadx.c",
+        "TaskP_threadx.c",
+        // DPL R5
+        "ClockP_threadx_r5.c",
+        "HwiP_armv7r_handlers_threadx.c",
+        // Threadx source
+        "tx_block_allocate.c",
+        "tx_block_pool_cleanup.c",
+        "tx_block_pool_create.c",
+        "tx_block_pool_delete.c",
+        "tx_block_pool_info_get.c",
+        "tx_block_pool_initialize.c",
+        "tx_block_pool_performance_info_get.c",
+        "tx_block_pool_performance_system_info_get.c",
+        "tx_block_pool_prioritize.c",
+        "tx_block_release.c",
+        "tx_byte_allocate.c",
+        "tx_byte_pool_cleanup.c",
+        "tx_byte_pool_create.c",
+        "tx_byte_pool_delete.c",
+        "tx_byte_pool_info_get.c",
+        "tx_byte_pool_initialize.c",
+        "tx_byte_pool_performance_info_get.c",
+        "tx_byte_pool_performance_system_info_get.c",
+        "tx_byte_pool_prioritize.c",
+        "tx_byte_pool_search.c",
+        "tx_byte_release.c",
+        "txe_block_allocate.c",
+        "txe_block_pool_create.c",
+        "txe_block_pool_delete.c",
+        "txe_block_pool_info_get.c",
+        "txe_block_pool_prioritize.c",
+        "txe_block_release.c",
+        "txe_byte_allocate.c",
+        "txe_byte_pool_create.c",
+        "txe_byte_pool_delete.c",
+        "txe_byte_pool_info_get.c",
+        "txe_byte_pool_prioritize.c",
+        "txe_byte_release.c",
+        "txe_event_flags_create.c",
+        "txe_event_flags_delete.c",
+        "txe_event_flags_get.c",
+        "txe_event_flags_info_get.c",
+        "txe_event_flags_set.c",
+        "txe_event_flags_set_notify.c",
+        "txe_mutex_create.c",
+        "txe_mutex_delete.c",
+        "txe_mutex_get.c",
+        "txe_mutex_info_get.c",
+        "txe_mutex_prioritize.c",
+        "txe_mutex_put.c",
+        "txe_queue_create.c",
+        "txe_queue_delete.c",
+        "txe_queue_flush.c",
+        "txe_queue_front_send.c",
+        "txe_queue_info_get.c",
+        "txe_queue_prioritize.c",
+        "txe_queue_receive.c",
+        "txe_queue_send.c",
+        "txe_queue_send_notify.c",
+        "txe_semaphore_ceiling_put.c",
+        "txe_semaphore_create.c",
+        "txe_semaphore_delete.c",
+        "txe_semaphore_get.c",
+        "txe_semaphore_info_get.c",
+        "txe_semaphore_prioritize.c",
+        "txe_semaphore_put.c",
+        "txe_semaphore_put_notify.c",
+        "txe_thread_create.c",
+        "txe_thread_delete.c",
+        "txe_thread_entry_exit_notify.c",
+        "txe_thread_info_get.c",
+        "txe_thread_preemption_change.c",
+        "txe_thread_priority_change.c",
+        "txe_thread_relinquish.c",
+        "txe_thread_reset.c",
+        "txe_thread_resume.c",
+        "txe_thread_suspend.c",
+        "txe_thread_terminate.c",
+        "txe_thread_time_slice_change.c",
+        "txe_thread_wait_abort.c",
+        "txe_timer_activate.c",
+        "txe_timer_change.c",
+        "txe_timer_create.c",
+        "txe_timer_deactivate.c",
+        "txe_timer_delete.c",
+        "txe_timer_info_get.c",
+        "tx_event_flags_cleanup.c",
+        "tx_event_flags_create.c",
+        "tx_event_flags_delete.c",
+        "tx_event_flags_get.c",
+        "tx_event_flags_info_get.c",
+        "tx_event_flags_initialize.c",
+        "tx_event_flags_performance_info_get.c",
+        "tx_event_flags_performance_system_info_get.c",
+        "tx_event_flags_set.c",
+        "tx_event_flags_set_notify.c",
+        "tx_initialize_high_level.c",
+        "tx_initialize_kernel_enter.c",
+        "tx_initialize_kernel_setup.c",
+        "tx_misra.c",
+        "tx_mutex_cleanup.c",
+        "tx_mutex_create.c",
+        "tx_mutex_delete.c",
+        "tx_mutex_get.c",
+        "tx_mutex_info_get.c",
+        "tx_mutex_initialize.c",
+        "tx_mutex_performance_info_get.c",
+        "tx_mutex_performance_system_info_get.c",
+        "tx_mutex_prioritize.c",
+        "tx_mutex_priority_change.c",
+        "tx_mutex_put.c",
+        "tx_queue_cleanup.c",
+        "tx_queue_create.c",
+        "tx_queue_delete.c",
+        "tx_queue_flush.c",
+        "tx_queue_front_send.c",
+        "tx_queue_info_get.c",
+        "tx_queue_initialize.c",
+        "tx_queue_performance_info_get.c",
+        "tx_queue_performance_system_info_get.c",
+        "tx_queue_prioritize.c",
+        "tx_queue_receive.c",
+        "tx_queue_send.c",
+        "tx_queue_send_notify.c",
+        "tx_semaphore_ceiling_put.c",
+        "tx_semaphore_cleanup.c",
+        "tx_semaphore_create.c",
+        "tx_semaphore_delete.c",
+        "tx_semaphore_get.c",
+        "tx_semaphore_info_get.c",
+        "tx_semaphore_initialize.c",
+        "tx_semaphore_performance_info_get.c",
+        "tx_semaphore_performance_system_info_get.c",
+        "tx_semaphore_prioritize.c",
+        "tx_semaphore_put.c",
+        "tx_semaphore_put_notify.c",
+        "tx_thread_create.c",
+        "tx_thread_delete.c",
+        "tx_thread_entry_exit_notify.c",
+        "tx_thread_identify.c",
+        "tx_thread_info_get.c",
+        "tx_thread_initialize.c",
+        "tx_thread_performance_info_get.c",
+        "tx_thread_performance_system_info_get.c",
+        "tx_thread_preemption_change.c",
+        "tx_thread_priority_change.c",
+        "tx_thread_relinquish.c",
+        "tx_thread_reset.c",
+        "tx_thread_resume.c",
+        "tx_thread_shell_entry.c",
+        "tx_thread_sleep.c",
+        "tx_thread_stack_analyze.c",
+        "tx_thread_stack_error_handler.c",
+        "tx_thread_stack_error_notify.c",
+        "tx_thread_suspend.c",
+        "tx_thread_system_preempt_check.c",
+        "tx_thread_system_resume.c",
+        "tx_thread_system_suspend.c",
+        "tx_thread_terminate.c",
+        "tx_thread_timeout.c",
+        "tx_thread_time_slice.c",
+        "tx_thread_time_slice_change.c",
+        "tx_thread_wait_abort.c",
+        "tx_time_get.c",
+        "tx_timer_activate.c",
+        "tx_timer_change.c",
+        "tx_timer_create.c",
+        "tx_timer_deactivate.c",
+        "tx_timer_delete.c",
+        "tx_timer_expiration_process.c",
+        "tx_timer_info_get.c",
+        "tx_timer_initialize.c",
+        "tx_timer_performance_info_get.c",
+        "tx_timer_performance_system_info_get.c",
+        "tx_timer_system_activate.c",
+        "tx_timer_system_deactivate.c",
+        "tx_timer_thread_entry.c",
+        "tx_time_set.c",
+        "tx_trace_buffer_full_notify.c",
+        "tx_trace_disable.c",
+        "tx_trace_enable.c",
+        "tx_trace_event_filter.c",
+        "tx_trace_event_unfilter.c",
+        "tx_trace_initialize.c",
+        "tx_trace_interrupt_control.c",
+        "tx_trace_isr_enter_insert.c",
+        "tx_trace_isr_exit_insert.c",
+        "tx_trace_object_register.c",
+        "tx_trace_object_unregister.c",
+        "tx_trace_user_event_insert.c"
+    ],
+};
+
+
+const asmfiles = {
+    common: [
+        // picked from nortos DPL
+        "boot_armv7r_asm.S",
+        "HwiP_armv7r_asm.S",
+        "MpuP_armv7r_asm.S",
+        "CacheP_armv7r_asm.S",
+        "CpuId_armv7r_asm.S",
+        "PmuP_armv7r_asm.S",
+        // DPL R5
+        "HwiP_armv7r_handlers_threadx_asm.S",
+        "HwiP_armv7r_vectors_threadx_asm.S",
+        // R5 port
+        "tx_thread_context_restore.S",
+        "tx_thread_context_save.S",
+        "tx_thread_fiq_context_restore.S",
+        "tx_thread_fiq_context_save.S",
+        "tx_thread_fiq_nesting_end.S",
+        "tx_thread_fiq_nesting_start.S",
+        "tx_thread_interrupt_control.S",
+        "tx_thread_interrupt_disable.S",
+        "tx_thread_interrupt_restore.S",
+        "tx_thread_irq_nesting_end.S",
+        "tx_thread_irq_nesting_start.S",
+        "tx_thread_schedule.S",
+        "tx_thread_stack_build.S",
+        "tx_thread_system_return.S",
+        "tx_thread_vectored_context_save.S",
+        "tx_timer_interrupt.S",
+    ],
+};
+
+const filedirs = {
+    common: [
+        // picked from nortos DPL
+        "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/dpl/r5",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/dpl/common",
+        "${MCU_PLUS_SDK_PATH}/source/eclipse_threadx/threadx/threadx_src/common/src",
+        "${MCU_PLUS_SDK_PATH}/source/eclipse_threadx/threadx/ports/ti_arm_gcc_clang_cortex_r5/src",
+        "${MCU_PLUS_SDK_PATH}/source/eclipse_threadx/threadx/dpl/common",
+        "${MCU_PLUS_SDK_PATH}/source/eclipse_threadx/threadx/dpl/r5",
+    ],
+};
+
+const includes = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/eclipse_threadx/threadx/ports/ti_arm_gcc_clang_cortex_r5/inc",
+        "${MCU_PLUS_SDK_PATH}/source/eclipse_threadx/threadx/threadx_src/common/inc",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/dpl",
+        "${MCU_PLUS_SDK_PATH}/source/eclipse_threadx/threadx/dpl/common"
+    ],
+};
+
+
+const cflags = {
+    common: [
+        "-Wno-extra",
+        "-Wno-uninitialized",
+        "-Wno-unused-but-set-variable",
+    ],
+};
+
+const buildOptionCombos = [
+    { device: device, cpu: "r5f", cgt: "ti-arm-clang"},
+    { device: device, cpu: "r5f", cgt: "gcc-armv7"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "library";
+    property.name = "threadx";
+    property.isInternal = false;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.filedirs = filedirs;
+    build_property.files = files;
+    build_property.asmfiles = asmfiles;
+    if(buildOption.cgt.match(/ti-arm-clang*/)) {
+        build_property.cflags = cflags;
+    }
+    if(buildOption.cpu.match(/r5f*/)) {
+        build_property.includes = includes;
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
