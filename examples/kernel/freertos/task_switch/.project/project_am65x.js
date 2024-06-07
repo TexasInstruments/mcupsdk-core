f9ad8f9ded31eada6362dea1c48c678fb316369b
Darshan M <darshan.m@ti.com>
2024-06-07 16:51:55 +0530

    am65x: r5f: Add freertos task_switch example
    1. .project:
       a. Update the task_switch example js in project_am65x.js.
    
    2. examples:
       a. Add project_am65x.js for task_switch example.
       b. Add the example syscfg for task_switch example.
    
    3.  Add task_switch example build in makefile
    
    Fixes: SITSW-2627
    
    Signed-off-by: Darshan M <darshan.m@ti.com>

---
+++ examples/kernel/freertos/task_switch/.project/project_am65x.js
@@ -0,0 +1,117 @@
+let path = require('path');
+
+let device = "am65x";
+
+const files = {
+    common: [
+        "task_switch.c",
+        "main.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",       /* core_os_combo base */
+        "../../..", /* Example base */
+    ],
+};
+
+const includes_r5f = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_ARM_CLANG/ARM_CR5F",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am65x/r5f",
+    ],
+};
+
+const libdirs = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+    ],
+};
+
+const libs_r5f = {
+    common: [
+        "freertos.am65x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am65x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const syscfgfile = "../example.syscfg";
+
+const readmeDoxygenPageTag = "EXAMPLES_KERNEL_FREERTOS_TASK_SWITCH";
+
+const templates_r5f =
+[
+    {
+        input: ".project/templates/am65x/common/linker_r5f.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am65x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "task_switch_main",
+        },
+    },
+];
+
+const buildOptionCombos = [
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am65x-idk", os: "freertos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "task_switch";
+    property.tirexResourceSubClass = [ "example.gettingstarted" ];
+    property.isInternal = false;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.libdirs = libdirs;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
+
+    if(buildOption.cpu.includes("r5f")) {
+        build_property.includes = includes_r5f;
+        if(buildOption.cgt.match(/gcc*/) )
+        {
+            build_property.templates = templates_r5f_gcc;
+            build_property.libs = libs_r5f_gcc;
+        }
+        else
+        {
+            build_property.templates = templates_r5f;
+            build_property.libs = libs_r5f;
+        }
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
