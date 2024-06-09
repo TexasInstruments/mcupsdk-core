35764b21792bdaedae34b2084c73edab3b7a682b
Jonathan Blanchard <jonathan.blanchard@jblopen.com>
2024-06-09 13:44:06 -0400

    am243x: threadx:  Add ThreadX DPL source files for R5.
    - Added DPL commong and R5 specific source files.
    
    Fixes: SITSW-5614
    
    Signed-off-by: Jonathan Blanchard <jonathan.blanchard@jblopen.com>

---
+++ source/eclipse_threadx/threadx/dpl/common/TaskP_threadx.c
@@ -0,0 +1,237 @@
+/*
+ *  Copyright (C) 2018-2024 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <kernel/dpl/TaskP.h>
+#include <kernel/dpl/ClockP.h>
+#include "tx_api.h"
+
+void vApplicationLoadHook(void);
+
+#define TaskP_LOAD_CPU_LOAD_SCALE   (10000U)
+#define TaskP_REGISTRY_MAX_ENTRIES  (32u)
+#define TaskP_STACK_SIZE_MIN        (128U)
+
+
+typedef struct TaskP_Struct_ {
+    TX_THREAD     taskObj;
+    void          *args;
+    TaskP_FxnMain taskMain;
+    uint32_t      lastRunTime;
+    uint64_t      accRunTime;
+} TaskP_Struct;
+
+typedef struct {
+    TaskP_Struct *taskRegistry[TaskP_REGISTRY_MAX_ENTRIES];
+    uint32_t lastTotalTime;
+    uint64_t accTotalTime;
+    uint32_t idleTskLastRunTime;
+    uint64_t idleTskAccRunTime;
+
+} TaskP_Ctrl;
+
+
+TaskP_Ctrl gTaskP_ctrl;
+
+static void TaskP_addToRegistry(TaskP_Struct *task)
+{
+    TX_INTERRUPT_SAVE_AREA
+    uint32_t i;
+
+    /* Disable interrupts. */
+    TX_DISABLE
+
+    for(i=0; i<TaskP_REGISTRY_MAX_ENTRIES; i++)
+    {
+        if(gTaskP_ctrl.taskRegistry[i]==NULL)
+        {
+            gTaskP_ctrl.taskRegistry[i] = task;
+            break;
+        }
+    }
+
+    /* Restore interrupts. */
+    TX_RESTORE
+}
+
+static void TaskP_removeFromRegistry(TaskP_Struct *task)
+{
+    TX_INTERRUPT_SAVE_AREA
+    uint32_t i;
+
+    /* Disable interrupts. */
+    TX_DISABLE
+
+    for(i=0; i<TaskP_REGISTRY_MAX_ENTRIES; i++)
+    {
+        if(gTaskP_ctrl.taskRegistry[i]==task)
+        {
+            gTaskP_ctrl.taskRegistry[i] = NULL;
+            break;
+        }
+    }
+
+    /* Restore interrupts. */
+    TX_RESTORE
+}
+
+static uint32_t TaskP_calcCounterDiff(uint32_t cur, uint32_t last)
+{
+    uint32_t delta;
+
+    if(cur >= last)
+    {
+        delta = cur - last;
+    }
+    else
+    {
+        delta = (  0xFFFFFFFFU - last ) + cur;
+    }
+    return delta;
+}
+
+static uint32_t TaskP_calcCpuLoad(uint64_t taskTime, uint64_t totalTime)
+{
+    uint32_t cpuLoad;
+
+    cpuLoad = (taskTime * TaskP_LOAD_CPU_LOAD_SCALE) / totalTime;
+    if( cpuLoad > TaskP_LOAD_CPU_LOAD_SCALE)
+    {
+        cpuLoad = TaskP_LOAD_CPU_LOAD_SCALE;
+    }
+    return cpuLoad;
+}
+
+static VOID task_wrapper(ULONG args)
+{
+    TaskP_Struct *task;
+
+    task = (TaskP_Struct *)args;
+
+    task->taskMain(task->args);
+}
+
+
+void TaskP_Params_init(TaskP_Params *params)
+{
+    params->name = "Task (DPL)";
+    params->stackSize = 0;
+    params->stack = NULL;
+    params->priority = ((TaskP_PRIORITY_HIGHEST - TaskP_PRIORITY_LOWEST) /(uint32_t) 2);
+    params->args = NULL;
+    params->taskMain = NULL;
+}
+
+int32_t TaskP_construct(TaskP_Object *obj, TaskP_Params *params)
+{
+    int32_t status = SystemP_SUCCESS;
+    TaskP_Struct *taskObj = (TaskP_Struct *)obj;
+    UINT tx_ret;
+
+    DebugP_assert(sizeof(TaskP_Struct) <= sizeof(TaskP_Object));
+    DebugP_assert(params != NULL);
+    DebugP_assert(taskObj != NULL);
+
+    DebugP_assert(params->stackSize >= TaskP_STACK_SIZE_MIN);
+    DebugP_assert( (params->stackSize & (sizeof(4u) - 1U)) == 0U); // TODO - Improve.
+    DebugP_assert(params->stack != NULL);
+    DebugP_assert( ((uintptr_t)params->stack & (sizeof(4u) - 1U)) == 0U); // TODO - Improve.
+
+    DebugP_assert(params->taskMain != NULL );
+
+    /* if prority is out of range, adjust to bring it in range */
+    if(params->priority > TaskP_PRIORITY_HIGHEST)
+    {
+        params->priority = TaskP_PRIORITY_HIGHEST;
+    }
+    if(params->priority <= TaskP_PRIORITY_LOWEST)
+    {
+        params->priority = TaskP_PRIORITY_LOWEST;
+    }
+
+    taskObj->lastRunTime = 0;
+    taskObj->accRunTime = 0;
+
+    TaskP_addToRegistry(taskObj);
+
+    taskObj->taskMain = params->taskMain;
+    taskObj->args = params->args;
+
+    tx_ret = tx_thread_create(&taskObj->taskObj,
+                              (CHAR *)params->name,
+                              task_wrapper,
+                              (ULONG)taskObj,
+                              params->stack,
+                              params->stackSize,
+                              params->priority,
+                              params->priority,
+                              0u,
+                              TX_TRUE);
+
+    if(tx_ret == TX_SUCCESS)
+    {
+        status = SystemP_FAILURE;
+    }
+
+    return status;
+}
+
+void TaskP_destruct(TaskP_Object *obj)
+{
+    TaskP_Struct *taskObj = (TaskP_Struct *)obj;
+
+    if(taskObj != NULL)
+    {
+        tx_thread_delete(&taskObj->taskObj);
+    }
+    if(taskObj != NULL)
+    {
+        TaskP_removeFromRegistry(taskObj);
+    }
+}
+
+void* TaskP_getHndl(TaskP_Object *obj)
+{
+    TaskP_Struct *taskObj = (TaskP_Struct *)obj;
+
+    return  (void*)&taskObj->taskObj;
+}
+
+void TaskP_yield(void)
+{
+    tx_thread_relinquish();
+}
+
+void TaskP_exit(void)
+{
+
+}
+
---
+++ source/eclipse_threadx/threadx/dpl/r5/HwiP_armv7r_handlers_threadx.c
@@ -0,0 +1,266 @@
+/*
+ *  Copyright (C) 2018-2024 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <kernel/dpl/HwiP.h>
+#include <kernel/nortos/dpl/r5/HwiP_armv7r_vim.h>
+#include <drivers/hw_include/csl_types.h>
+#include <drivers/hw_include/soc_config.h>
+#include <drivers/pmu.h>
+
+static volatile uint32_t gdummy;
+
+
+/* compile flag to enable or disable interrupt nesting */
+#define HWIP_NESTED_INTERRUPTS_IRQ_ENABLE
+
+/* Save FPU context, used in FIQ Handler */
+static inline  void Hwip_save_fpu_context(void)
+{
+    __asm__ __volatile__ ( "FMRX  R0, FPSCR"  "\n\t": : : "memory");
+    __asm__ __volatile__ ( "VPUSH {D0-D15}"  "\n\t": : : "memory");
+    __asm__ __volatile__ ( "PUSH  {R0}"  "\n\t": : : "memory");
+}
+
+/* Restore FPU context, used in FIQ Handler */
+static inline  void Hwip_restore_fpu_context(void)
+{
+    __asm__ __volatile__ ( "POP   {R0}"  "\n\t": : : "memory");
+    __asm__ __volatile__ ( "VPOP  {D0-D15}"  "\n\t": : : "memory");
+    __asm__ __volatile__ ( "VMSR  FPSCR, R0"  "\n\t": : : "memory");
+}
+
+void __attribute__((section(".text.hwi"))) HwiP_irq_handler_c(void);
+void __attribute__((interrupt("UNDEF"), section(".text.hwi"))) HwiP_reserved_handler(void);
+void __attribute__((interrupt("UNDEF"), section(".text.hwi"))) HwiP_undefined_handler(void);
+void __attribute__((interrupt("ABORT"), section(".text.hwi"))) HwiP_prefetch_abort_handler(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
+void __attribute__((interrupt("ABORT"), section(".text.hwi"))) HwiP_data_abort_handler(void);
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef INTR_PROF
+void __attribute__((section(".text.hwi"))) HwiP_irq_profile_c(uint32_t intNum)
+{
+    uint32_t idx = 0, exists = 1;
+
+    for(idx = 0; idx <= gHwiCtrlProf.traceInterruptedISRIndex; idx++)
+    {
+        if(gHwiCtrlProf.traceInterruptedISR[idx] == intNum)
+        {
+            gHwiCtrlProf.traceInterruptedISR[idx] = 0;
+            gHwiCtrlProf.traceInterruptedISRIndex --;
+            if(gHwiCtrlProf.traceInterruptedISRIndex == 0)
+            {
+                gHwiCtrlProf.readCounterStop = CSL_armR5PmuReadCntr(CSL_ARM_R5_PMU_CYCLE_COUNTER_NUM);
+                if(gHwiCtrlProf.readCounterStop > gHwiCtrlProf.readCounterStart)
+                {
+                    gHwiCtrlProf.pmuCountVal += gHwiCtrlProf.readCounterStop - gHwiCtrlProf.readCounterStart - gHwiCtrlProf.pmuCalibration;
+                }
+                else
+                {
+                    gHwiCtrlProf.pmuCountVal += (0xFFFFFFFFU - gHwiCtrlProf.readCounterStart) + gHwiCtrlProf.readCounterStop - gHwiCtrlProf.pmuCalibration;
+                }
+            }
+            exists = 0;
+            break;
+        }
+    }
+    if(exists)
+    {
+        gHwiCtrlProf.traceInterruptedISR[(gHwiCtrlProf.traceInterruptedISRIndex)] = intNum;
+        gHwiCtrlProf.traceInterruptedISRIndex ++;
+        gHwiCtrlProf.readCounterStart = CSL_armR5PmuReadCntr(CSL_ARM_R5_PMU_CYCLE_COUNTER_NUM);
+    }
+}
+#endif
+
+/* IRQ handler starts execution in HwiP_irq_handler, defined in portASM.S
+ * After some initial assembly logic it then branches to this function.
+ * After exiting this function it does some more assembly including
+ * doing task switch if requested.
+ */
+void __attribute__((section(".text.hwi"))) HwiP_irq_handler_c(void)
+{
+    int32_t status;
+    uint32_t intNum;
+
+    #ifndef HWIP_VIM_VIC_ENABLE
+    volatile uint32_t gdummy;
+
+    /* Read to force prioritization logic to take effect */
+    gdummy = HwiP_getIRQVecAddr();
+    #endif
+
+    status = HwiP_getIRQ(&intNum);
+
+    if(status==SystemP_SUCCESS)
+    {
+        #ifdef INTR_PROF
+        if(gHwiCtrlProf.profileIntr == 1)
+        {
+            HwiP_irq_profile_c(intNum);
+        }
+        #endif
+
+        uint32_t isPulse = HwiP_isPulse(intNum);
+        HwiP_FxnCallback isr;
+        void *args;
+
+        if(isPulse != 0U)
+        {
+            HwiP_clearInt(intNum);
+        }
+
+        isr = gHwiCtrl.isr[intNum];
+        args = gHwiCtrl.isrArgs[intNum];
+
+        #ifdef HWIP_NESTED_INTERRUPTS_IRQ_ENABLE
+        /* allow nesting of interrupts */
+        HwiP_enable();
+        #endif
+
+        if(isr!=NULL)
+        {
+            isr(args);
+        }
+
+        /* disallow nesting of interrupts */
+        (void)HwiP_disable();
+
+        if(isPulse == 0U)
+        {
+            HwiP_clearInt(intNum);
+        }
+        HwiP_ackIRQ(intNum);
+
+        #ifdef INTR_PROF
+        if(gHwiCtrlProf.profileIntr == 1)
+        {
+            HwiP_irq_profile_c(intNum);
+        }
+        #endif
+    }
+    else
+    {
+        /* spurious interrupt */
+        gHwiCtrl.spuriousIRQCount++;
+        HwiP_ackIRQ(0);
+    }
+}
+
+void __attribute__((interrupt("FIQ"), section(".text.hwi"))) HwiP_fiq_handler(void)
+{
+    int32_t status;
+    uint32_t intNum;
+
+    #ifdef EN_SAVE_RESTORE_FPU_CONTEXT
+    Hwip_save_fpu_context();
+    #endif
+
+    /* Read to force prioritization logic to take effect */
+    gdummy = HwiP_getFIQVecAddr();
+
+    status = HwiP_getFIQ(&intNum);
+    if(status==SystemP_SUCCESS)
+    {
+        uint32_t isPulse = HwiP_isPulse(intNum);
+        HwiP_FxnCallback isr;
+        void *args;
+
+        if(isPulse != 0U)
+        {
+            HwiP_clearInt(intNum);
+        }
+
+        isr = gHwiCtrl.isr[intNum];
+        args = gHwiCtrl.isrArgs[intNum];
+
+        #if 0   /* FIQ interrupt nesting not supported */
+        /* allow nesting of interrupts */
+        HwiP_enableFIQ();
+        #endif
+
+        if(isr!=NULL)
+        {
+            isr(args);
+        }
+
+        /* disallow nesting of interrupts */
+        (void)HwiP_disableFIQ();
+
+        if(isPulse == 0U)
+        {
+            HwiP_clearInt(intNum);
+        }
+        HwiP_ackFIQ(intNum);
+    }
+    else
+    {
+        /* spurious interrupt */
+        gHwiCtrl.spuriousFIQCount++;
+        HwiP_ackFIQ(0);
+    }
+
+    #ifdef EN_SAVE_RESTORE_FPU_CONTEXT
+    Hwip_restore_fpu_context();
+    #endif
+}
+
+void __attribute__((interrupt("UNDEF"), section(".text.hwi"))) HwiP_reserved_handler(void)
+{
+    volatile uint32_t loop = 1;
+    while(loop != 0U) { ; }
+
+}
+
+void __attribute__((interrupt("UNDEF"), section(".text.hwi"))) HwiP_undefined_handler(void)
+{
+    volatile uint32_t loop = 1;
+    while(loop != 0U){  ; }
+
+}
+
+void __attribute__((interrupt("ABORT"), section(".text.hwi"))) HwiP_prefetch_abort_handler(void)
+{
+    volatile uint32_t loop = 1;
+    while(loop != 0U)  { ;}
+}
+
+void __attribute__((interrupt("ABORT"), section(".text.hwi"), weak)) HwiP_data_abort_handler_c(void)
+{
+    volatile uint32_t loop = 1;
+    while(loop != 0U){ ; }
+
+}
