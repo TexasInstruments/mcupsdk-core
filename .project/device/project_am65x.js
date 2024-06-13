1719c0d8a29592e54066fed1ced11be69d8f4271
Darshan M <darshan.m@ti.com>
2024-06-13 15:03:23 +0530

    am65x: r5f: Add I2C driver test case.
    1. .project:
       Update the I2C driver test js in project_am65x.js.
    
    2. test:
       a. Add the project_am65x.js for I2C driver test case.
       b. Add the syscfg for I2C driver test case.
       C. Add the board.c for I2C driver test case.
    
    3. Add I2C driver test case build in makefile.
    
    Fixes: SITSW-2634
    
    Signed-off-by: Darshan M <darshan.m@ti.com>

--- .project/device/project_am65x.js
+++ .project/device/project_am65x.js
@@ -29,6 +29,7 @@ const example_file_list = [
     "examples/kernel/freertos/posix_demo/.project/project.js",
     "examples/kernel/freertos/task_switch/.project/project.js",
     "test/drivers/gpio/.project/project.js",
+    "test/drivers/i2c/.project/project.js",
     "test/drivers/sciclient/.project/project.js",
     "test/kernel/dpl/.project/project.js",
     "test/kernel/freertos/.project/project.js",
