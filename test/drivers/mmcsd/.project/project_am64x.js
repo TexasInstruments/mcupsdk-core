bff34c00786e57ec9f945a130522c699d0927e61
Rohith R <rohith.rajan@ti.com>
2024-06-14 19:30:05 +0530

    am64x: a53: mmcsd: unit test
        * Add build support for the mmcsd unit test on a53 in am64x-evm
    
    Fixes: SITSW-4654
    
    Signed-off-by: Rohith R <rohith.rajan@ti.com>

--- test/drivers/mmcsd/.project/project_am64x.js
+++ test/drivers/mmcsd/.project/project_am64x.js
@@ -39,6 +39,28 @@ const includes = {
     ],
 };
 
+const includes_nortos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/FreeRTOS-FAT/include",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable/nortos",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/config",
+        "${MCU_PLUS_SDK_PATH}/test/unity/",
+    ],
+};
+
+const includes_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/GCC/ARM_CA53",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/FreeRTOS-FAT/include",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable/nortos",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/config",
+        "${MCU_PLUS_SDK_PATH}/test/unity/",
+    ],
+};
+
 const libs_r5f = {
     common: [
         "nortos.am64x.r5f.ti-arm-clang.${ConfigName}.lib",
@@ -48,6 +70,41 @@ const libs_r5f = {
     ],
 };
 
+const libdirs_nortos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/lib",
+        "${MCU_PLUS_SDK_PATH}/test/unity/lib",
+    ],
+};
+
+const libs_nortos_a53 = {
+    common: [
+        "nortos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "freertos_fat.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "unity.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+    ],
+};
+
+const libdirs_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/lib",
+        "${MCU_PLUS_SDK_PATH}/test/unity/lib",
+    ],
+};
+
+const libs_freertos_a53 = {
+    common: [
+        "drivers.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "freertos_fat.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "unity.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+    ],
+};
 
 const lnkfiles = {
     common: [
@@ -68,8 +125,40 @@ const templates_nortos_r5f =
     }
 ];
 
+const templates_nortos_a53 =
+[
+    {
+        input: ".project/templates/am64x/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am64x/nortos/main_nortos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "test_main",
+        },
+    },
+];
+
+const templates_freertos_a53 =
+[
+    {
+        input: ".project/templates/am64x/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am64x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "test_main",
+        },
+    }
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-evm", os: "nortos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am64x-evm", os: "nortos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am64x-evm", os: "freertos"},
 ];
 
 function getComponentProperty() {
@@ -99,7 +188,19 @@ function getComponentBuildProperty(buildOption) {
         build_property.libs = libs_r5f;
         build_property.templates = templates_nortos_r5f;
     }
-
+    if(buildOption.cpu.match(/a53*/)) {
+        build_property.libs = libs_nortos_a53;
+        build_property.templates = templates_nortos_a53;
+        build_property.includes = includes_nortos_a53;
+        build_property.libdirs = libdirs_nortos_a53;
+        if(buildOption.os.match(/freertos*/) )
+        {
+            build_property.includes = includes_freertos_a53;
+            build_property.libdirs = libdirs_freertos_a53;
+            build_property.libs = libs_freertos_a53;
+            build_property.templates = templates_freertos_a53;
+        }
+    }
     return build_property;
 }
 
