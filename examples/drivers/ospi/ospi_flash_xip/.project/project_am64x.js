5f72423915e0ae266eb1f03b59c5cd751d0bcde0
Rohith R <rohith.rajan@ti.com>
2024-06-19 11:07:42 +0530

    am64x: a53: ospi: ospi_flash_xip
    	* Add build support for the ospi_flash_xip example
    	  on a53 running freertos and nortos in am64x-evm
    	* Update the documentation for this example
    
    Fixes: SITSW-4783
    
    Signed-off-by: Rohith R <rohith.rajan@ti.com>

--- examples/drivers/ospi/ospi_flash_xip/.project/project_am64x.js
+++ examples/drivers/ospi/ospi_flash_xip/.project/project_am64x.js
@@ -35,6 +35,46 @@ const libs_r5f = {
     ],
 };
 
+const includes_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/GCC/ARM_CA53",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am64x/a53",
+    ],
+};
+
+const libdirs_freertos_a53 = {
+	common: [
+		"${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+		"${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+		"${MCU_PLUS_SDK_PATH}/source/board/lib",
+	],
+};
+
+const libs_freertos_a53 = {
+    common: [
+        "freertos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "board.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+    ],
+};
+
+const libdirs_nortos_a53 = {
+	common: [
+		"${MCU_PLUS_SDK_PATH}/source/kernel/nortos/lib",
+		"${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+		"${MCU_PLUS_SDK_PATH}/source/board/lib",
+	],
+};
+
+const libs_nortos_a53 = {
+    common: [
+        "nortos.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+        "board.am64x.a53.gcc-aarch64.${ConfigName}.lib",
+    ],
+};
+
 const lnkfiles = {
     common: [
         "linker.cmd",
@@ -56,9 +96,41 @@ const templates_nortos_r5f =
     }
 ];
 
+const templates_freertos_a53 =
+[
+    {
+        input: ".project/templates/am64x/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am64x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "ospi_flash_xip_main",
+        },
+    }
+];
+
+const templates_nortos_a53 =
+[
+    {
+        input: ".project/templates/am64x/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am64x/nortos/main_nortos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "ospi_flash_xip_main",
+        },
+    }
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-evm", os: "nortos"},
-    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-sk", os: "nortos"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am64x-sk",  os: "nortos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am64x-evm", os: "freertos"},
+	{ device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am64x-evm", os: "nortos"},
 ];
 
 function getComponentProperty() {
@@ -86,6 +158,21 @@ function getComponentBuildProperty(buildOption) {
     if(buildOption.cpu.match(/r5f*/)) {
         build_property.libs = libs_r5f;
         build_property.templates = templates_nortos_r5f;
+    }
+    if(buildOption.cpu.match(/a53*/)){
+		if(buildOption.os.match(/freertos*/)) {
+			build_property.includes = includes_freertos_a53;
+			build_property.libdirs = libdirs_freertos_a53;
+			build_property.libs = libs_freertos_a53;
+			build_property.templates = templates_freertos_a53;
+		}
+		else {
+			build_property.libdirs = libdirs_nortos_a53;
+			build_property.libs = libs_nortos_a53;
+			build_property.templates = templates_nortos_a53;
+		}
+
+
     }
 
     return build_property;
