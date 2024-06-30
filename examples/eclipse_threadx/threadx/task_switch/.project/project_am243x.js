7d5dacb8466ee166a36fe9e13ab1417f71a929d8
Edouard Haas <edouard.haas@jblopen.com>
2024-06-30 15:39:20 -0400

    am243x: threadx: Add support for launchpad.
    - Added the relevant build combos (threadx+launchpad) to individual test and example project files.
    - Added the missing example.syscfg files for each test/example.
    
    Fixes: SITSW-5614
    
    Signed-off-by: Edouard Haas <edouard.haas@jblopen.com>

--- examples/eclipse_threadx/threadx/task_switch/.project/project_am243x.js
+++ examples/eclipse_threadx/threadx/task_switch/.project/project_am243x.js
@@ -95,6 +95,8 @@ const templates =
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-evm", os: "threadx"},
     { device: device, cpu: "r5fss0-0", cgt: "gcc-armv7",    board: "am243x-evm", os: "threadx"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-lp", os: "threadx"},
+    { device: device, cpu: "r5fss0-0", cgt: "gcc-armv7",    board: "am243x-lp", os: "threadx"},
 ];
 
 function getComponentProperty() {
