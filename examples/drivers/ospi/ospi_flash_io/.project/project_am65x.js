17bc93e3c729c5011cbed653069ed289c7db4f82
Suryaprakash Rao <suryaprakashraog@ti.com>
2024-07-02 19:49:56 +0530

    am65x: r5f: Add OSPI flash io example
    1. .project:
       Update the ospi flash io example js in project_am65x js.
    
    2. examples:
       a. Add the ospi flash io example js file.
       b. Add the example syscfg for ospi flash io example.
    
    3. Add ospi flash io example build in makefile.
    
    Fixes: SITSW-2637
    
    Signed-off-by: Suryaprakash Rao <suryaprakashraog@ti.com>

---
+++ examples/drivers/ospi/ospi_flash_io/.project/project_am65x.js
@@ -0,0 +1,100 @@
+let path = require('path');
+
+let device = "am65x";
+
+const files = {
+    common: [
+        "ospi_flash_io.c",
+        "main.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",       /* core_os_combo base */
+        "../../..", /* Example base */
+    ],
+};
+
+const libdirs = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+    ],
+};
+
+const libs_r5f = {
+    common: [
+        "nortos.am65x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am65x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.am65x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const syscfgfile = "../example.syscfg"
+
+const readmeDoxygenPageTag = "EXAMPLES_DRIVERS_OSPI_FLASH_IO";
+
+const templates_nortos_r5f =
+[
+    {
+        input: ".project/templates/am65x/common/linker_r5f.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am65x/nortos/main_nortos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "ospi_flash_io_main",
+        },
+    }
+];
+
+const buildOptionCombos = [
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am65x-idk", os: "nortos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "ospi_flash_io";
+    property.isInternal = false;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.libdirs = libdirs;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
+
+    if(buildOption.cpu.match(/r5f*/)) {
+        build_property.libs = libs_r5f;
+        build_property.templates = templates_nortos_r5f;
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
