6138faa3c80b63013db81f61442ceac9d0eba8e5
Piyush Sharma <p-sharma3@ti.com>
2024-07-17 17:10:58 +0530

    am243x/am64x: Freertos: Add FreeRTOS FAT example
     - FreeRTOS_FAT is now supported with FreeRTOS as well as
     NORTOS
    
    Fixes: SITREQ-4681
    
    Signed-off-by: Piyush Sharma <p-sharma3@ti.com>

--- examples/drivers/mmcsd/mmcsd_file_io/.project/project_am243x.js
+++ examples/drivers/mmcsd/mmcsd_file_io/.project/project_am243x.js
@@ -19,7 +19,7 @@ const filedirs = {
     ],
 };
 
-const includes_r5f = {
+const includes_nortos_r5f = {
     common: [
         "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/FreeRTOS-FAT/include",
         "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable",
@@ -28,7 +28,18 @@ const includes_r5f = {
     ],
 };
 
-const libdirs = {
+const includes_freertos_r5f = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_ARM_CLANG/ARM_CR5F",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/FreeRTOS-FAT/include",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/portable/nortos",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/config",
+    ],
+};
+
+const libdirs_nortos = {
     common: [
         "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/lib",
         "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
@@ -36,7 +47,15 @@ const libdirs = {
     ],
 };
 
-const libs_r5f = {
+const libdirs_freertos = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/fs/freertos_fat/lib",
+    ],
+};
+
+const libs_nortos_r5f = {
     common: [
         "nortos.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
         "drivers.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
@@ -44,9 +63,25 @@ const libs_r5f = {
     ],
 };
 
-const libs_r5f_gcc = {
+const libs_nortos_r5f_gcc = {
     common: [
         "nortos.am243x.r5f.gcc-armv7.${ConfigName}.lib",
+        "drivers.am243x.r5f.gcc-armv7.${ConfigName}.lib",
+        "freertos_fat.am243x.r5f.gcc-armv7.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_r5f = {
+    common: [
+        "freertos.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "freertos_fat.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_r5f_gcc = {
+    common: [
+        "freertos.am243x.r5f.gcc-armv7.${ConfigName}.lib",
         "drivers.am243x.r5f.gcc-armv7.${ConfigName}.lib",
         "freertos_fat.am243x.r5f.gcc-armv7.${ConfigName}.lib",
     ],
@@ -91,9 +126,40 @@ const templates_nortos_r5f_gcc =
     }
 ];
 
+const templates_freertos_r5f =
+[
+    {
+        input: ".project/templates/am243x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "mmcsd_file_io_main",
+        },
+    }
+];
+
+const templates_freertos_r5f_gcc =
+[
+    {
+        input: ".project/templates/am243x/common/linker_r5f_gcc.cmd.xdt",
+        output: "linker.cmd",
+        options: {
+            isSingleCore: true,
+        },
+    },
+    {
+        input: ".project/templates/am243x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "mmcsd_file_io_main",
+        },
+    }
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-evm", os: "nortos"},
     { device: device, cpu: "r5fss0-0", cgt: "gcc-armv7", board: "am243x-evm", os: "nortos"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-evm", os: "freertos"},
+    { device: device, cpu: "r5fss0-0", cgt: "gcc-armv7", board: "am243x-evm", os: "freertos"},
 ];
 
 function getComponentProperty() {
@@ -113,23 +179,41 @@ function getComponentBuildProperty(buildOption) {
 
     build_property.files = files;
     build_property.filedirs = filedirs;
-    build_property.libdirs = libdirs;
     build_property.lnkfiles = lnkfiles;
     build_property.syscfgfile = syscfgfile;
     build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
 
     if(buildOption.cpu.match(/r5f*/)) {
-        if(buildOption.cgt.match(/gcc*/) )
+        if(buildOption.os.match(/freertos*/) )
         {
-            build_property.libs = libs_r5f_gcc;
-            build_property.templates = templates_nortos_r5f_gcc;
+            build_property.includes = includes_freertos_r5f;
+            build_property.libdirs = libdirs_freertos;
+            if(buildOption.cgt.match(/gcc*/) )
+            {
+                build_property.libs = libs_freertos_r5f_gcc;
+                build_property.templates = templates_freertos_r5f_gcc;
+            }
+            else
+            {
+                build_property.libs = libs_freertos_r5f;
+                build_property.templates = templates_freertos_r5f;
+            }
         }
         else
         {
-            build_property.libs = libs_r5f;
-            build_property.templates = templates_nortos_r5f;
+            build_property.includes = includes_nortos_r5f;
+            build_property.libdirs = libdirs_nortos;
+            if(buildOption.cgt.match(/gcc*/) )
+            {
+                build_property.libs = libs_nortos_r5f_gcc;
+                build_property.templates = templates_nortos_r5f_gcc;
+            }
+            else
+            {
+                build_property.libs = libs_nortos_r5f;
+                build_property.templates = templates_nortos_r5f;
+            }
         }
-        build_property.includes = includes_r5f;
     }
 
     return build_property;
