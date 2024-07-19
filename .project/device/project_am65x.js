374648347b0df0df7837944697c038b94b9ea5e0
Darshan M <darshan.m@ti.com>
2024-07-19 18:24:25 +0530

    am65x: r5f: Add MCSPI performance 32bit example.
    1.  .project:
        Update the MCSPI performance 32bit example js in project_am65x js.
    
    2.  examples:
        a. Add the MCSPI performance 32bit example js file.
        b. Add the MCSPI performance 32bit example syscfg
    
    3.  Update makefile and projectspec makefile
    
    Fixes: SITSW-2635
    
    Signed-off-by: Darshan M <darshan.m@ti.com>

--- .project/device/project_am65x.js
+++ .project/device/project_am65x.js
@@ -34,6 +34,7 @@ const example_file_list = [
     "examples/drivers/ipc/ipc_rpmsg_echo_linux/.project/project.js",
     "examples/drivers/mcspi/mcspi_loopback/.project/project.js",
     "examples/drivers/mcspi/mcspi_performance_8bit/.project/project.js",
+    "examples/drivers/mcspi/mcspi_performance_32bit/.project/project.js",
     "examples/drivers/mmcsd/mmcsd_file_io/.project/project.js",
     "examples/drivers/mmcsd/mmcsd_raw_io/.project/project.js",
     "examples/drivers/ospi/ospi_flash_diag/.project/project.js",
