37e2711a37f9933fd9f8da7594cf3628c71652db
Pradeep HN <p-hn@ti.com>
2024-07-30 21:07:57 +0530

    am263x: enet: Enable enet SBL load
    Signed-off-by: Pradeep HN <p-hn@ti.com>

--- .project/device/project_am263x.js
+++ .project/device/project_am263x.js
@@ -29,6 +29,7 @@ const device_defines = {
 };
 
 const example_file_list = [
+    "examples/drivers/boot/sbl_qspi_enet/.project/project.js",
     "examples/drivers/adc/adc_multiple_soc_epwm/.project/project.js",
     "examples/drivers/adc/adc_ppb_epwm_trip/.project/project.js",
     "examples/drivers/adc/adc_ppb_delay/.project/project.js",
