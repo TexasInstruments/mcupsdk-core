dd18bfcc3766338dda8c757b5895dd31ec28a569
Abishek SS <abishekss@ti.com>
2024-07-30 17:41:19 +0530

    am261x: pmic: remove blackbird pmic
      - remove blackbird specific pmic files from am261x board makefile
    
    Fixes: MCUSDK-13469
    Signed-off-by: Abishek SS <abishekss@ti.com>

--- source/board/.project/project_am261x.js
+++ source/board/.project/project_am261x.js
@@ -20,15 +20,6 @@ const files_r5f = {
         "nor_spi_sfdp.c",
         "ioexp_tca6424.c",
         "ioexp_tca6416.c",
-        "pmic_tps653860xx.c",
-        "pmic.c",
-        "pmic_core.c",
-        "pmic_esm.c",
-        "pmic_fsm.c",
-        "pmic_gpio.c",
-        "pmic_io.c",
-        "pmic_power.c",
-        "pmic_wdg.c",
     ],
 };
 
@@ -59,8 +50,6 @@ const includes = {
         "pmic",
         "pmic/pmic_lld/src",
         "pmic/pmic_lld/include",
-        "pmic/pmic_lld/include/cfg/tps65386x",
-        "pmic/pmic_lld/src/cfg/tps65386x",
     ],
 };
 
