5736a78a9e7171485466ade454957a09eac02f92
Sanmveg Saini <a0500224@ti.com>
2024-08-09 14:05:13 +0530

    am263x: SBL_QSPI: Add different flavours of SBL
    Create new SBL_QSPI with default OOB features:
    1. sbl_qspi: backward compatible
    2.  sbl_qspi_multicore_elf: support MulticoreELF
    image boot
    3. sbl_ospi_fastboot: support fast boot option
    
    Fixes: MCUSDK-13518
    
    Signed-off-by: sanmveg saini <sanmveg@ti.com>

--- examples/drivers/boot/sbl_qspi_fastboot/.project/project_am263x.js
+++ examples/drivers/boot/sbl_qspi_fastboot/.project/project_am263x.js
@@ -58,14 +58,14 @@ const includes = {
 };
 
 const template_options = {
-    bootformat: "RPRC",
-    enableFastBoot: false,
+    bootformat: "MCELF",
+    enableFastBoot: true,
 }
 
 const templates =
 [
     {
-        input: ".project/templates/am263x/sbl/sbl_qspi/main_backsupport.c.xdt",
+        input: ".project/templates/am263x/sbl/sbl_qspi/main_fastboot.c.xdt",
         output: "../main.c",
         options: template_options
     }
@@ -73,7 +73,7 @@ const templates =
 
 const syscfgfile = "../example.syscfg";
 
-const readmeDoxygenPageTag = "EXAMPLES_DRIVERS_SBL_QSPI";
+const readmeDoxygenPageTag = "EXAMPLES_DRIVERS_SBL_QSPI_FAST";
 
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am263x-cc", os: "nortos"},
@@ -85,7 +85,7 @@ function getComponentProperty() {
 
     property.dirPath = path.resolve(__dirname, "..");
     property.type = "executable";
-    property.name = "sbl_qspi";
+    property.name = "sbl_qspi_fastboot";
     property.isInternal = false;
     property.isBootLoader = true;
     property.buildOptionCombos = buildOptionCombos;
