2caad9c14965ef908fc9a8cc5547ece15bcc398e
Aswathi <a-r1@ti.com>
2024-08-14 17:46:04 +0530

    am261x: infra: fix build for jenkins job
    - fsi, canfd, tmu, enet examples disable
    - fix param in soc_xbar.h
    - remove pmic from adc_dac, adc_open_shorts examples
    - remove 1_0, 1_1 cores from multicore examples
    - boot, enet tests disable
    
    Fixes: MCUSDK-13617
    Signed-off-by: Aswathi R <a-r1@ti.com>

--- source/drivers/soc/am261x/soc_xbar.h
+++ source/drivers/soc/am261x/soc_xbar.h
@@ -509,6 +509,7 @@ SOC_xbarInvertOutputXBarOutputSignal(uint32_t base, uint32_t invertout)
  * \param group7_mask [in] Mask to OR inputs from group 7
  * \param group8_mask [in] Mask to OR inputs from group 8
  * \param group9_mask [in] Mask to OR inputs from group 9
+ * \param group10_mask [in] Mask to OR inputs from group 10
  *
  */
 static inline void
