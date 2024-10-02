f520be0d64ca2b75f57d0d7eb94711d6d3ef1885
Edouard Haas <edouard.haas@jblopen.com>
2024-10-01 23:16:54 -0400

    am243x: netxduo: add tcp client/server and udp client examples for netxduo
    - add source and project files for udp client and tcp client/server examples
    - added syscfg file for evm and lp in switch mode (dual rx channel)
    - modify am243x device project file to include all 3 examples
    
    Fixes: SITSW-5614
    Signed-off-by: Edouard Haas <edouard.haas@jblopen.com>

---
+++ examples/networking/netxduo/enet_netxduo_cpsw_udp_client/app_cpswconfighandler.c
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2001,2002 Florian Schulze.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the authors nor the names of the contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * app_cpswconfighandler.c - This file is part of lwIP test
+ *
+ */
+/* ========================================================================== */
+/*                             Include Files                                  */
+/* ========================================================================== */
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+/* SDK includes */
+#include <networking/enet/utils/include/enet_apputils.h>
+#include <networking/enet/utils/include/enet_board.h>
+#include <enet.h>
+#include <kernel/dpl/TaskP.h>
+#include <kernel/dpl/ClockP.h>
+#include <kernel/dpl/QueueP.h>
+#include "ti_board_config.h"
+#include "ti_board_open_close.h"
+#include "ti_drivers_open_close.h"
+#include "ti_enet_config.h"
+#include "ti_enet_open_close.h"
+
+/* ========================================================================== */
+/*                           Macros & Typedefs                                */
+/* ========================================================================== */
+
+/* ========================================================================== */
+/*                         Structure Declarations                             */
+/* ========================================================================== */
+
+/* ========================================================================== */
+/*                          Function Declarations                             */
+/* ========================================================================== */
+
+
+static void EnetApp_mdioLinkStatusChange(Cpsw_MdioLinkStateChangeInfo *info,
+                                         void *appArg);
+
+static void EnetApp_portLinkStatusChangeCb(Enet_MacPort macPort,
+                                           bool isLinkUp,
+                                           void *appArg);
+
+/* ========================================================================== */
+/*                            Global Variables                                */
+/* ========================================================================== */
+
+/* ========================================================================== */
+/*                          Function Definitions                              */
+/* ========================================================================== */
+
+void EnetApp_addMCastEntry(Enet_Type enetType,
+                          uint32_t instId,
+                          uint32_t coreId,
+                          const uint8_t *testMCastAddr,
+                          uint32_t portMask)
+{
+    Enet_IoctlPrms prms;
+    int32_t status;
+    CpswAle_SetMcastEntryInArgs setMcastInArgs;
+    uint32_t setMcastOutArgs;
+
+    if (Enet_isCpswFamily(enetType))
+    {
+        Enet_Handle hEnet = Enet_getHandle(enetType, instId);
+
+        EnetAppUtils_assert(hEnet != NULL);
+        memset(&setMcastInArgs, 0, sizeof(setMcastInArgs));
+        memcpy(&setMcastInArgs.addr.addr[0U], testMCastAddr,
+               sizeof(setMcastInArgs.addr.addr));
+        setMcastInArgs.addr.vlanId  = 0;
+        setMcastInArgs.info.super = false;
+        setMcastInArgs.info.numIgnBits = 0;
+        setMcastInArgs.info.fwdState = CPSW_ALE_FWDSTLVL_FWD;
+        setMcastInArgs.info.portMask = portMask;
+        ENET_IOCTL_SET_INOUT_ARGS(&prms, &setMcastInArgs, &setMcastOutArgs);
+        ENET_IOCTL(hEnet, coreId, CPSW_ALE_IOCTL_ADD_MCAST, &prms, status);
+        if (status != ENET_SOK)
+        {
+           EnetAppUtils_print("EnetTestBcastMcastLimit_AddAleEntry() failed CPSW_ALE_IOCTL_ADD_MCAST: %d\n",
+                               status);
+        }
+    }
+}
+
+
+void EnetApp_updateCpswInitCfg(Enet_Type enetType, uint32_t instId, Cpsw_Cfg *cpswCfg)
+{
+#if defined (ENET_SOC_HOSTPORT_DMA_TYPE_CPDMA)
+    EnetCpdma_Cfg * dmaCfg = (EnetCpdma_Cfg *)cpswCfg->dmaCfg;
+
+    EnetAppUtils_assert(dmaCfg != NULL);
+    EnetAppUtils_assert(EnetAppUtils_isDescCached() == false);
+    dmaCfg->rxInterruptPerMSec = 8;
+    dmaCfg->txInterruptPerMSec = 2;
+#endif
+
+
+#if (ENET_SYSCFG_ENABLE_MDIO_MANUALMODE == 1U)
+    cpswCfg->mdioLinkStateChangeCb    = NULL;
+    cpswCfg->mdioLinkStateChangeCbArg = NULL;
+#else
+    cpswCfg->mdioLinkStateChangeCb    = &EnetApp_mdioLinkStatusChange;
+    cpswCfg->mdioLinkStateChangeCbArg = NULL;
+#endif
+    cpswCfg->portLinkStatusChangeCb = &EnetApp_portLinkStatusChangeCb;
+    cpswCfg->portLinkStatusChangeCbArg = NULL;
+}
+
+static void EnetApp_mdioLinkStatusChange(Cpsw_MdioLinkStateChangeInfo *info,
+                                         void *appArg)
+{
+    if (info->linkChanged)
+    {
+        EnetAppUtils_print("Link Status Changed. PHY: 0x%x, state: %s\r\n",
+                info->phyAddr,
+                info->isLinked? "up" : "down");
+    }
+}
+
+static void EnetApp_portLinkStatusChangeCb(Enet_MacPort macPort,
+                                           bool isLinkUp,
+                                           void *appArg)
+{
+    EnetAppUtils_print("MAC Port %u: link %s\r\n",
+                       ENET_MACPORT_ID(macPort), isLinkUp ? "up" : "down");
+}
