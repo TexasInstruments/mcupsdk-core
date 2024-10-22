5946e5308c9be0ddfb3938e1d1cd0d91673159f9
Edouard Haas <edouard.haas@jblopen.com>
2024-10-22 15:25:23 -0400

    am243x: filex: fix filex/levelx nor driver
    - replace old driver open/init code incompatible with sysconfig generated init code.
    
    Fixes: SITSW-5614
    Signed-off-by: Edouard Haas <edouard.haas@jblopen.com>
    (cherry picked from commit dd74dd042bd34fa80a5f25b2f94c196a52f5e9e6)

--- source/fs/filex/filex_levelx/lx_nor_flash_driver.h
+++ source/fs/filex/filex_levelx/lx_nor_flash_driver.h
@@ -1,4 +1,34 @@
-
+/*
+ *  Copyright (C) 2018-2024 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef LX_NOR_FLASH_DRIVER_H
 #define LX_NOR_FLASH_DRIVER_H
@@ -6,12 +36,10 @@
 #include <stdint.h>
 #include <lx_api.h>
 
-UINT  lx_nor_driver_create(uint32_t nor_driver_instance_id, uint32_t flash_instance_id, uint32_t offset, uint32_t size);
+UINT lx_nor_driver_create(LX_NOR_FLASH *p_nor_flash, uint32_t flash_instance_id, uint32_t offset, uint32_t size);
 
-UINT lx_nor_driver_init(LX_NOR_FLASH *p_nand_flash);
+UINT lx_nor_driver_init(LX_NOR_FLASH *p_nor_flash);
 
-uint32_t lx_nor_driver_size_get(uint32_t nor_driver_instance_id);
-
-LX_NOR_FLASH *lx_nor_driver_nor_flash_get(uint32_t nor_driver_instance_id);
+uint64_t lx_nor_driver_size_get(LX_NOR_FLASH *p_nor_flash);
 
 #endif
