75a4d566f4a45274e3edde689c3e623e2abbe9f0
Edouard Haas <edouard.haas@jblopen.com>
2024-10-22 15:25:23 -0400

    am243x: filex: fix filex/levelx nor driver
    - replace old driver open/init code incompatible with sysconfig generated init code.
    
    Fixes: SITSW-5614
    Signed-off-by: Edouard Haas <edouard.haas@jblopen.com>
    (cherry picked from commit dd74dd042bd34fa80a5f25b2f94c196a52f5e9e6)

--- source/fs/filex/filex_levelx/filex_levelx_nor_driver.c
+++ source/fs/filex/filex_levelx/filex_levelx_nor_driver.c
@@ -1,31 +1,39 @@
-/***************************************************************************
- * Copyright (c) 2024 Microsoft Corporation 
- * 
- * This program and the accompanying materials are made available under the
- * terms of the MIT License which is available at
- * https://opensource.org/licenses/MIT.
- * 
- * SPDX-License-Identifier: MIT
- **************************************************************************/
-
-
-/**************************************************************************/
-/**************************************************************************/
-/**                                                                       */ 
-/** FileX Component                                                       */ 
-/**                                                                       */
-/**   FileX NOR FLASH Simulator Driver                                    */
-/**                                                                       */
-/**************************************************************************/
-/**************************************************************************/
-
-
-/* Include necessary system files.  */
+/*
+ *  Copyright (C) 2018-2024 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "fx_api.h"
 #include "lx_api.h"
 
-#include <fs/filex/filex_levelx/lx_nor_flash_driver.h>
+#include <lx_nor_flash_driver.h>
 
 
 VOID _fx_nor_driver(FX_MEDIA *media_ptr)
@@ -37,84 +45,12 @@ VOID _fx_nor_driver(FX_MEDIA *media_ptr)
     ULONG i;
     UINT status;
 
-
-    /* There are several useful/important pieces of information contained in the media
-       structure, some of which are supplied by FileX and others are for the driver to
-       setup. The following is a summary of the necessary FX_MEDIA structure members:
-
-            FX_MEDIA Member                              Meaning
-
-        fx_media_driver_request             FileX request type. Valid requests from FileX are 
-                                            as follows:
-
-                                                    FX_DRIVER_READ
-                                                    FX_DRIVER_WRITE                 
-                                                    FX_DRIVER_FLUSH
-                                                    FX_DRIVER_ABORT
-                                                    FX_DRIVER_INIT
-                                                    FX_DRIVER_BOOT_READ
-                                                    FX_DRIVER_RELEASE_SECTORS
-                                                    FX_DRIVER_BOOT_WRITE
-                                                    FX_DRIVER_UNINIT
-
-        fx_media_driver_status              This value is RETURNED by the driver. If the 
-                                            operation is successful, this field should be
-                                            set to FX_SUCCESS for before returning. Otherwise,
-                                            if an error occurred, this field should be set
-                                            to FX_IO_ERROR. 
-
-        fx_media_driver_buffer              Pointer to buffer to read or write sector data.
-                                            This is supplied by FileX.
-
-        fx_media_driver_logical_sector      Logical sector FileX is requesting.
-
-        fx_media_driver_sectors             Number of sectors FileX is requesting.
-
-
-       The following is a summary of the optional FX_MEDIA structure members:
-
-            FX_MEDIA Member                              Meaning
-
-        fx_media_driver_info                Pointer to any additional information or memory.
-                                            This is optional for the driver use and is setup
-                                            from the fx_media_open call. The RAM disk uses
-                                            this pointer for the RAM disk memory itself.
-
-        fx_media_driver_write_protect       The DRIVER sets this to FX_TRUE when media is write 
-                                            protected. This is typically done in initialization, 
-                                            but can be done anytime.
-
-        fx_media_driver_free_sector_update  The DRIVER sets this to FX_TRUE when it needs to 
-                                            know when clusters are released. This is important
-                                            for FLASH wear-leveling drivers.
-
-        fx_media_driver_system_write        FileX sets this flag to FX_TRUE if the sector being
-                                            written is a system sector, e.g., a boot, FAT, or 
-                                            directory sector. The driver may choose to use this
-                                            to initiate error recovery logic for greater fault
-                                            tolerance.
-
-        fx_media_driver_data_sector_read    FileX sets this flag to FX_TRUE if the sector(s) being
-                                            read are file data sectors, i.e., NOT system sectors.
-
-        fx_media_driver_sector_type         FileX sets this variable to the specific type of 
-                                            sector being read or written. The following sector
-                                            types are identified:
-
-                                                    FX_UNKNOWN_SECTOR 
-                                                    FX_BOOT_SECTOR
-                                                    FX_FAT_SECTOR
-                                                    FX_DIRECTORY_SECTOR
-                                                    FX_DATA_SECTOR  
-     */
-
-
     /* Process the driver request specified in the media control block.  */
     switch(media_ptr -> fx_media_driver_request) {
 
         case FX_DRIVER_READ:
 
-            p_nor_flash = lx_nor_driver_nor_flash_get((uint32_t)media_ptr->fx_media_driver_info);
+            p_nor_flash = (LX_NOR_FLASH *)media_ptr->fx_media_driver_info;
 
             /* Setup the destination buffer and logical sector.  */
             logical_sector =      media_ptr -> fx_media_driver_logical_sector;
@@ -148,7 +84,7 @@ VOID _fx_nor_driver(FX_MEDIA *media_ptr)
 
         case FX_DRIVER_WRITE:
 
-            p_nor_flash = lx_nor_driver_nor_flash_get((uint32_t)media_ptr->fx_media_driver_info);
+            p_nor_flash = (LX_NOR_FLASH *)media_ptr->fx_media_driver_info;
 
             /* Setup the source buffer and logical sector.  */
             logical_sector =      media_ptr -> fx_media_driver_logical_sector;
@@ -182,7 +118,7 @@ VOID _fx_nor_driver(FX_MEDIA *media_ptr)
 
         case FX_DRIVER_RELEASE_SECTORS:
 
-            p_nor_flash = lx_nor_driver_nor_flash_get((uint32_t)media_ptr->fx_media_driver_info);
+            p_nor_flash = (LX_NOR_FLASH *)media_ptr->fx_media_driver_info;
 
             /* Setup the logical sector.  */
             logical_sector =  media_ptr -> fx_media_driver_logical_sector;
@@ -225,6 +161,7 @@ VOID _fx_nor_driver(FX_MEDIA *media_ptr)
             break;
 
         case FX_DRIVER_INIT:
+
             media_ptr -> fx_media_driver_status =  FX_SUCCESS;
             break;
 
@@ -234,7 +171,7 @@ VOID _fx_nor_driver(FX_MEDIA *media_ptr)
 
         case FX_DRIVER_BOOT_READ:
 
-            p_nor_flash = lx_nor_driver_nor_flash_get((uint32_t)media_ptr->fx_media_driver_info);
+            p_nor_flash = (LX_NOR_FLASH *)media_ptr->fx_media_driver_info;
 
             /* Read the boot record and return to the caller.  */
 
@@ -271,7 +208,7 @@ VOID _fx_nor_driver(FX_MEDIA *media_ptr)
 
         case FX_DRIVER_BOOT_WRITE:
 
-            p_nor_flash = lx_nor_driver_nor_flash_get((uint32_t)media_ptr->fx_media_driver_info);
+            p_nor_flash = (LX_NOR_FLASH *)media_ptr->fx_media_driver_info;
 
             /* Make sure the media bytes per sector equals to the LevelX logical sector size.  */
             if (media_ptr -> fx_media_bytes_per_sector != (LX_NOR_SECTOR_SIZE) * sizeof(ULONG))
--- source/fs/filex/filex_levelx/lx_nor_flash_driver.c
+++ source/fs/filex/filex_levelx/lx_nor_flash_driver.c
@@ -1,29 +1,47 @@
-/***************************************************************************
- * Copyright (c) 2024 Microsoft Corporation 
- * 
- * This program and the accompanying materials are made available under the
- * terms of the MIT License which is available at
- * https://opensource.org/licenses/MIT.
- * 
- * SPDX-License-Identifier: MIT
- **************************************************************************/
+/*
+ *  Copyright (C) 2018-2024 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
-/**************************************************************************/
-/**************************************************************************/
-/**                                                                       */ 
-/** LevelX Component                                                      */ 
-/**                                                                       */
-/**   NOR Flash Simulator                                                 */
-/**                                                                       */
-/**************************************************************************/
-/**************************************************************************/
+/* ========================================================================== */
+/*                             Include Files                                  */
+/* ========================================================================== */
 
-
-/* Include necessary files.  */
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #include "lx_api.h"
 #include "lx_nor_flash_driver.h"
+#include <tx_api.h>
 
 #include <board/flash.h>
 #include <kernel/dpl/DebugP.h>
@@ -32,51 +50,79 @@
 #error "Control block mode must be enabled."
 #endif
 
-/* Define constants for the NOR flash simulation. */
 
-/* This configuration is for one physical sector of overhead.  */
-
-
-#define LX_NOR_FLASH_DRIVER_GET(p_nor)      ((lx_nor_driver_t *)((uint8_t *)(p_nor) - (((uint8_t *)&((lx_nor_driver_t *)0)->nor_flash) - (uint8_t *)((lx_nor_driver_t *)0))))
+/* ========================================================================== */
+/*                           Macros & Typedefs                                */
+/* ========================================================================== */
 
 
-#define LX_NOR_DRIVER_MAX_INSTANCE_COUNT      (4u)
+/* Maximum number of this driver instances. */
+#define LX_NOR_DRIVER_MAX_INSTANCE_COUNT         (4u)
 
+#define LX_NOR_DRIVER_ERASED_VERIFY_BUF_SIZE   (256u)
 
 #define FREE_BIT_MAP_WORDS                  ((PHYSICAL_SECTORS_PER_BLOCK-1)/32)+1
 #define USABLE_SECTORS_PER_BLOCK            (PHYSICAL_SECTORS_PER_BLOCK-1)
 #define UNUSED_METADATA_WORDS_PER_BLOCK     (WORDS_PER_PHYSICAL_SECTOR-(3+FREE_BIT_MAP_WORDS+USABLE_SECTORS_PER_BLOCK))
 
+
+/* ========================================================================== */
+/*                         Structure Declarations                             */
+/* ========================================================================== */
+
+/* NOR driver instance. */
 typedef struct lx_nor_driver {
-    LX_NOR_FLASH nor_flash;
+    LX_NOR_FLASH *p_nor_flash;
     Flash_Handle flash_handle;
     Flash_Attrs *p_attrs;
-    uint8_t t_sec_buf[512u];
     uint32_t offset;
     uint32_t size;
 } lx_nor_driver_t;
 
 
+/* Global NOR driver data. */
 typedef struct lx_nor_driver_data {
     lx_nor_driver_t t_instances[LX_NOR_DRIVER_MAX_INSTANCE_COUNT];
+    size_t instance_cnt;
+    TX_MUTEX buf_mutex;
+    bool init;
 } lx_nor_driver_data_t;
 
 
+/* ========================================================================== */
+/*                            Global Variables                                */
+/* ========================================================================== */
+
 static lx_nor_driver_data_t g_nor_driver_data = {0};
+static uint8_t gt_sec_bufs[LX_NOR_DRIVER_MAX_INSTANCE_COUNT][512] __attribute((aligned(128)));
+static uint8_t gt_erased_verify_buf[LX_NOR_DRIVER_ERASED_VERIFY_BUF_SIZE] __attribute((aligned(128)));
 
-UINT  lx_nor_driver_read(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *destination, ULONG words);
-UINT  lx_nor_driver_write(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *source, ULONG words);
-UINT  lx_nor_driver_block_erase(LX_NOR_FLASH *nor_flash, ULONG block, ULONG erase_count);
-UINT  lx_nor_driver_block_erased_verify(LX_NOR_FLASH *nor_flash, ULONG block);
-UINT  lx_nor_driver_system_error(LX_NOR_FLASH *nor_flash, UINT error_code, ULONG block, ULONG sector);
+/* ========================================================================== */
+/*                          Function Declarations                             */
+/* ========================================================================== */
+
+static UINT  lx_nor_driver_read(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *destination, ULONG words);
+static UINT  lx_nor_driver_write(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *source, ULONG words);
+static UINT  lx_nor_driver_block_erase(LX_NOR_FLASH *nor_flash, ULONG block, ULONG erase_count);
+static UINT  lx_nor_driver_block_erased_verify(LX_NOR_FLASH *nor_flash, ULONG block);
+static UINT  lx_nor_driver_system_error(LX_NOR_FLASH *nor_flash, UINT error_code, ULONG block, ULONG sector);
+
+static lx_nor_driver_t *lx_nor_driver_get(LX_NOR_FLASH *p_nor_flash);
 
 
-UINT  lx_nor_driver_create(uint32_t nor_driver_instance_id, uint32_t flash_instance_id, uint32_t offset, uint32_t size)
+/* ========================================================================== */
+/*                            Function Definitions                            */
+/* ========================================================================== */
+
+UINT  lx_nor_driver_create(LX_NOR_FLASH *p_nor_flash, uint32_t flash_instance_id, uint32_t offset, uint32_t size)
 {
     lx_nor_driver_t *p_nor_driver;
+    UINT res;
 
-    p_nor_driver = &g_nor_driver_data.t_instances[nor_driver_instance_id];
+    p_nor_driver = &g_nor_driver_data.t_instances[g_nor_driver_data.instance_cnt];
+    g_nor_driver_data.instance_cnt++;
 
+    p_nor_driver->p_nor_flash = p_nor_flash;
     p_nor_driver->p_attrs = Flash_getAttrs(flash_instance_id);
     p_nor_driver->flash_handle = Flash_getHandle(flash_instance_id);
 
@@ -84,17 +130,26 @@ UINT  lx_nor_driver_create(uint32_t nor_driver_instance_id, uint32_t flash_insta
     DebugP_assert((size == (uint32_t)-1) || (size % p_nor_driver->p_attrs->blockSize == 0));
 
     p_nor_driver->offset = offset;
-    p_nor_driver->size = size == (uint32_t)-1 ? p_nor_driver->p_attrs->flashSize : size;
+    p_nor_driver->size = size == (uint32_t)-1 ? p_nor_driver->p_attrs->flashSize - offset : size;
 
-    return(LX_SUCCESS);
+    if (!g_nor_driver_data.init) {
+        g_nor_driver_data.init = true;
+        res = tx_mutex_create(&g_nor_driver_data.buf_mutex, "lx_nor_driver_buf_mutex", TX_INHERIT);
+        if (res != SystemP_SUCCESS) {
+            return (LX_ERROR);
+        }
+    }
+
+    return (LX_SUCCESS);
 }
 
 
 UINT lx_nor_driver_init(LX_NOR_FLASH *p_nor_flash)
 {
     lx_nor_driver_t *p_nor_driver;
+    size_t driver_ix;
 
-    p_nor_driver = LX_NOR_FLASH_DRIVER_GET(p_nor_flash);
+    p_nor_driver = lx_nor_driver_get(p_nor_flash);
 
     DebugP_assert(p_nor_driver - &g_nor_driver_data.t_instances[0] < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
     DebugP_assert(p_nor_driver->p_attrs != NULL);
@@ -113,34 +168,31 @@ UINT lx_nor_driver_init(LX_NOR_FLASH *p_nor_flash)
     p_nor_flash->lx_nor_flash_driver_block_erased_verify = lx_nor_driver_block_erased_verify;
 
     /* Setup local buffer for NOR flash operation. This buffer must be the sector size of the NOR flash memory.  */
-    p_nor_flash->lx_nor_flash_sector_buffer = (ULONG *)&p_nor_driver->t_sec_buf[0];
+    driver_ix = p_nor_driver - &g_nor_driver_data.t_instances[0];
+    p_nor_flash->lx_nor_flash_sector_buffer = (ULONG *)&gt_sec_bufs[driver_ix][0];
 
     return (LX_SUCCESS);
 }
 
 
-uint32_t lx_nor_driver_size_get(uint32_t nor_driver_instance_id)
+uint64_t lx_nor_driver_size_get(LX_NOR_FLASH *nor_flash)
 {
-    DebugP_assert(nor_driver_instance_id < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
-    DebugP_assert(g_nor_driver_data.t_instances[nor_driver_instance_id].p_attrs != NULL);
-    return (g_nor_driver_data.t_instances[nor_driver_instance_id].size);
+    lx_nor_driver_t *p_nor_driver;
+
+    p_nor_driver = lx_nor_driver_get(nor_flash);
+
+    DebugP_assert(p_nor_driver - &g_nor_driver_data.t_instances[0] < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
+    DebugP_assert(p_nor_driver->p_attrs != NULL);
+
+    return (p_nor_driver->size);
 }
 
-
-LX_NOR_FLASH *lx_nor_driver_nor_flash_get(uint32_t nor_driver_instance_id)
-{
-    DebugP_assert(nor_driver_instance_id < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
-    DebugP_assert(g_nor_driver_data.t_instances[nor_driver_instance_id].p_attrs != NULL);
-    return (&g_nor_driver_data.t_instances[nor_driver_instance_id].nor_flash);
-}
-
-
-UINT lx_nor_driver_read(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *destination, ULONG words)
+static UINT lx_nor_driver_read(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *destination, ULONG words)
 {
     lx_nor_driver_t *p_nor_driver;
     int32_t ret;
 
-    p_nor_driver = LX_NOR_FLASH_DRIVER_GET(nor_flash);
+    p_nor_driver = lx_nor_driver_get(nor_flash);
 
     DebugP_assert(p_nor_driver - &g_nor_driver_data.t_instances[0] < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
     DebugP_assert(p_nor_driver->p_attrs != NULL);
@@ -152,12 +204,12 @@ UINT lx_nor_driver_read(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *de
 }
 
 
-UINT lx_nor_driver_write(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *source, ULONG words)
+static UINT lx_nor_driver_write(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *source, ULONG words)
 {
     lx_nor_driver_t *p_nor_driver;
     int32_t ret;
 
-    p_nor_driver = LX_NOR_FLASH_DRIVER_GET(nor_flash);
+    p_nor_driver = lx_nor_driver_get(nor_flash);
 
     DebugP_assert(p_nor_driver - &g_nor_driver_data.t_instances[0] < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
     DebugP_assert(p_nor_driver->p_attrs != NULL);
@@ -169,14 +221,14 @@ UINT lx_nor_driver_write(LX_NOR_FLASH *nor_flash, ULONG *flash_address, ULONG *s
 }
 
 
-UINT lx_nor_driver_block_erase(LX_NOR_FLASH *nor_flash, ULONG block, ULONG erase_count)
+static UINT lx_nor_driver_block_erase(LX_NOR_FLASH *nor_flash, ULONG block, ULONG erase_count)
 {
     lx_nor_driver_t *p_nor_driver;
     int32_t ret;
 
     (void)erase_count;
 
-    p_nor_driver = LX_NOR_FLASH_DRIVER_GET(nor_flash);
+    p_nor_driver = lx_nor_driver_get(nor_flash);
 
     DebugP_assert(p_nor_driver - &g_nor_driver_data.t_instances[0] < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
     DebugP_assert(p_nor_driver->p_attrs != NULL);
@@ -188,25 +240,50 @@ UINT lx_nor_driver_block_erase(LX_NOR_FLASH *nor_flash, ULONG block, ULONG erase
 }
 
 
-UINT lx_nor_driver_block_erased_verify(LX_NOR_FLASH *nor_flash, ULONG block)
+static UINT lx_nor_driver_block_erased_verify(LX_NOR_FLASH *nor_flash, ULONG block)
 {
     lx_nor_driver_t *p_nor_driver;
+    uint32_t base_offset;
+    uint32_t rel_offset;
     int32_t ret;
+    UINT res;
 
-    p_nor_driver = LX_NOR_FLASH_DRIVER_GET(nor_flash);
+    p_nor_driver = lx_nor_driver_get(nor_flash);
 
-    DebugP_assert(p_nor_driver - &g_nor_driver_data.t_instances[0] < LX_NOR_DRIVER_MAX_INSTANCE_COUNT);
-    DebugP_assert(p_nor_driver->p_attrs != NULL);
+    res = tx_mutex_get(&g_nor_driver_data.buf_mutex, TX_WAIT_FOREVER);
+    if (res != TX_SUCCESS) {
+        return (LX_ERROR);
+    }
 
-    ret = Flash_eraseBlk(p_nor_driver->flash_handle, block + p_nor_driver->offset / p_nor_driver->p_attrs->blockSize);
-    if (ret != SystemP_SUCCESS) return (LX_ERROR);
+    rel_offset = 0u;
+    base_offset = p_nor_driver->offset + block * p_nor_driver->p_attrs->blockSize;
+    while (rel_offset < p_nor_driver->p_attrs->blockSize) {
 
-    // TODO: Verify here?
+        ret = Flash_read(p_nor_driver->flash_handle, base_offset + rel_offset, &gt_erased_verify_buf[0], LX_NOR_DRIVER_ERASED_VERIFY_BUF_SIZE);
+        if (ret != SystemP_SUCCESS) {
+            (void)tx_mutex_put(&g_nor_driver_data.buf_mutex);
+            return (LX_ERROR);
+        }
+
+        for (size_t k = 0u; k < LX_NOR_DRIVER_ERASED_VERIFY_BUF_SIZE / 4u; k++) {
+            if (((uint32_t *)gt_erased_verify_buf)[k] != 0xFFFFFFFF) {
+                (void)tx_mutex_put(&g_nor_driver_data.buf_mutex);
+                return (LX_ERROR);
+            }
+        }
+
+        rel_offset += LX_NOR_DRIVER_ERASED_VERIFY_BUF_SIZE;
+    }
+
+    res = tx_mutex_put(&g_nor_driver_data.buf_mutex);
+    if (res != TX_SUCCESS) {
+        return (LX_ERROR);
+    }
 
     return (LX_SUCCESS);
 }
 
-UINT lx_nor_driver_system_error(LX_NOR_FLASH *nor_flash, UINT error_code, ULONG block, ULONG sector)
+static UINT lx_nor_driver_system_error(LX_NOR_FLASH *nor_flash, UINT error_code, ULONG block, ULONG sector)
 {
 
 #ifdef LX_NOR_ENABLE_CONTROL_BLOCK_FOR_DRIVER_INTERFACE
@@ -218,5 +295,16 @@ UINT lx_nor_driver_system_error(LX_NOR_FLASH *nor_flash, UINT error_code, ULONG 
 
     /* Custom processing goes here...  all errors are fatal.  */
     return(LX_ERROR);
+}
+
+
+static lx_nor_driver_t *lx_nor_driver_get(LX_NOR_FLASH *p_nor_flash)
+{
+    for (size_t k = 0u; k < LX_NOR_DRIVER_MAX_INSTANCE_COUNT; k++) {
+        if (g_nor_driver_data.t_instances[k].p_nor_flash == p_nor_flash) {
+            return (&g_nor_driver_data.t_instances[k]);
+        }
+    }
+    return (NULL);
 }
 
