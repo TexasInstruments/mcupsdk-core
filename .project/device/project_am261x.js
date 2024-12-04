9de4eb5fc1683eaba7c9845a8d0c89c6c9471dc9
Ashwin Raj <ashwin.raj@ti.com>
2024-12-04 21:37:53 +0530

    am261x: tools: add flasher algorithm support
      - add flasher application support for am261x lp and som board which
    can be used by the CCS/Uniflash to flash the image onto the target
    
    Fixes: MCUSDK-13878
    Signed-off-by: Abishek SS <abishekss@ti.com>

--- .project/device/project_am261x.js
+++ .project/device/project_am261x.js
@@ -395,6 +395,7 @@ const example_file_list = [
 	"test/security/crypto/test_dthe_aes_stream_state_test/.project/project.js",
 	"test/security/crypto/test_dthe_hmac_sha/.project/project.js",
 	"test/security/crypto/test_dthe_sha/.project/project.js",
+	"tools/flasher/jtag_uniflash/.project/project.js",
 ];
 
 function getProjectSpecCpu(cpu) {
---
+++ tools/flasher/jtag_uniflash/.project/project_am261x.js
@@ -0,0 +1,95 @@
+let path = require('path');
+
+let device = "am261x";
+
+const files = {
+    common: [
+        "main.c",
+        "board.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",       /* core_os_combo base */
+        "../../..", /* Example base */
+    ],
+};
+
+const libdirs_nortos = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+        "${MCU_PLUS_SDK_PATH}/source/middleware/lib",
+    ],
+};
+
+const libs_nortos_r5f = {
+    common: [
+        "nortos.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "middleware.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const includes = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/security",
+    ],
+};
+
+const syscfgfile = "../example.syscfg";
+
+const readmeDoxygenPageTag = "TOOLS_UNIFLASH_CUSTOM_FLASHER";
+
+const buildOptionCombos = [
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am261x-som", os: "nortos"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am261x-lp", os: "nortos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "flasher_jtag_uniflash";
+    property.isInternal = true;
+    property.isBootLoader = true;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.libdirs = libdirs_nortos;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
+
+    if(buildOption.cpu.match(/r5f*/)) {
+        build_property.libs = libs_nortos_r5f;
+    }
+    build_property.includes = includes;
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
