5e076e17417cf8bc3fc4b9e1f21dbd8439016f01
Sanmveg Saini <a0500224@ti.com>
2024-12-05 13:10:00 +0530

    am263px: optiflash: Update overlay example
    Update function overlay exmaple with more comments
    and fixed logical bugs.
    
    Fixes: MCUSDK-14023
    
    Signed-off-by: sanmveg saini <sanmveg@ti.com>

--- docs_src/docs/api_guide/examples/benchmarks/benchmark_sram_overlay.md
+++ docs_src/docs/api_guide/examples/benchmarks/benchmark_sram_overlay.md
@@ -16,13 +16,19 @@
 
 # Introduction
 
-- This demo provides a rough measurement of the CPU cycles taken for a given code execution which is located in different memory regions (L2OCRAM and Flash memory) and also shows the performance when utilizing the overlay feature.
+- This demo provides a rough measurement of the CPU cycles taken for a given code execution (Measured in CPI) which is located in different memory regions (L2OCRAM and Flash memory) and also shows the performance when utilizing the overlay feature.
 - The example does the following:
   1. Initializes the drivers and board
   2. Initializes the cycle counters for counting the CPU cycles.
   3. The triggers the FLC(fast Local Copy) HW to copy the code from flash to RAM.
   4. Three iterations are done one with code executing from ram next from flash(XIP) and third with FLC overlay.
   5. Average cycles for code execution is calculated in example.
+
+## Performance optimization for overlay
+
+\note Here, code does not wait for function to be copied entirely in the internal RAM. 
+
+Looking at the sample output shows that, in case, when CPI of a function is less than 5, then, code should wait for function to get copied else it is really not required to wait for copy to complete and execution of the function can be done right away.
 
 # Steps to Run the Example
 
@@ -43,32 +49,40 @@ Flash the application binary to the device, follow the steps mentioned here
 
 \cond SOC_AM263PX
 \code
+
 Starting OSPI Bootloader ... 
-[BOOTLOADER_PROFILE] Boot Media       : NOR SPI FLASH 
+KPI_DATA: [BOOTLOADER_PROFILE] Boot Media       : NOR SPI FLASH 
 KPI_DATA: [BOOTLOADER_PROFILE] Boot Media Clock : 133.333 MHz 
 KPI_DATA: [BOOTLOADER_PROFILE] Boot Image Size  : 98 KB 
-[BOOTLOADER_PROFILE] Cores present    : 
+KPI_DATA: [BOOTLOADER_PROFILE] Cores present    : 
 r5f0-0
-KPI_DATA: [BOOTLOADER PROFILE] System_init                      :        647us 
-KPI_DATA: [BOOTLOADER PROFILE] Drivers_open                     :        118us 
-KPI_DATA: [BOOTLOADER PROFILE] LoadHsmRtFw                      :       8500us 
-KPI_DATA: [BOOTLOADER PROFILE] Board_driversOpen                :      33858us 
-KPI_DATA: [BOOTLOADER PROFILE] CPU load                         :         81us 
-KPI_DATA: [BOOTLOADER_PROFILE] SBL Total Time Taken             :      45369us 
+KPI_DATA: [BOOTLOADER PROFILE] System_init                      :        541us 
+KPI_DATA: [BOOTLOADER PROFILE] Drivers_open                     :        126us 
+KPI_DATA: [BOOTLOADER PROFILE] LoadHsmRtFw                      :       9373us 
+KPI_DATA: [BOOTLOADER PROFILE] Board_driversOpen                :       2751us 
+KPI_DATA: [BOOTLOADER PROFILE] CPU load                         :       5308us 
+KPI_DATA: [BOOTLOADER PROFILE] SBL End                          :         20us 
+KPI_DATA: [BOOTLOADER_PROFILE] SBL Total Time Taken             :      18123us 
 
 Image loading done, switching to application ...
-T1 SRAM: 74458, 9962, 801, 5700
-T1 XIP : 141912, 10123, 826, 5837
-T1 OVL : 1256926, 204701, 756, 132094
-T2 SRAM: 40798, 6634, 410, 4150
-T2 XIP : 64829, 6634, 411, 4157
-T2 XIP : 1044332, 201263, 321, 130260
-T3 SRAM: 25889, 5354, 263, 3574
-T3 XIP : 25877, 5354, 260, 3583
-T3 XIP : 979075, 199876, 241, 129730
-T4 SRAM: 17505, 5608, 315, 3769
-T4 XIP : 20543, 5608, 314, 3767
-T4 XIP : 985577, 200399, 210, 130078
+
+Test started
+Label          :CPU Cycles     CPU Instr.     CPI
+T1 SRAM        :74444          9962           7.5 
+T1 XIP         :142542         10097          14.1 
+T1 OVL         :75501          10031          7.5 
+
+T2 SRAM        :40882          6634           6.2 
+T2 XIP         :65101          6634           9.8 
+T2 OVL         :42501          6703           6.3 
+
+T3 SRAM        :26013          5354           4.9 
+T3 XIP         :25910          5354           4.8 
+T3 OVL         :36757          5423           6.8 
+
+T4 SRAM        :17539          5608           3.1 
+T4 XIP         :21282          5608           3.8 
+T4 OVL         :50720          5677           8.9 
 All tests have passed!!
 
 BENCHMARK END
--- examples/benchmarks/sram_overlay_benchmark/sram_overlay.c
+++ examples/benchmarks/sram_overlay_benchmark/sram_overlay.c
@@ -31,46 +31,211 @@
  */
 
 #include <stdio.h>
-#include <kernel/dpl/DebugP.h>
-#include <kernel/dpl/TaskP.h>
-#include <kernel/dpl/QueueP.h>
 #include <kernel/dpl/SemaphoreP.h>
 #include <drivers/optiflash.h>
 #include "ti_drivers_config.h"
 #include "ti_drivers_open_close.h"
 #include "ti_board_open_close.h"
-#include "FreeRTOS.h"
-#include "queue.h"
-#include "task.h"
-
+#include <drivers/hw_include/am263px/cslr_rl2_of_r5fss0_core0.h>
 #include "csl_arm_r5_pmu.h"
 #include "appprofile.h"
 #include "load.h"
 
+/**
+ * @brief Minimum FLC region size
+ * 
+ */
+#define APP_MINIMUM_FLC_REGION_SIZE             (32*1024U)
+
+/**
+ * @brief Number of peripheral register read done by load function 1
+ * 
+ * the actual number of reads are, this number, times 256
+ * 
+ */
+#define LOAD_FNX1_READ_REGISTER_CNT         4
+/**
+ * @brief Number of peripheral register writes done by load function 1
+ * 
+ */
+#define LOAD_FNX1_WRITE_REGISTER_CNT        1
+/**
+ * @brief Number of computation type A done by load function 1
+ * 
+ */
+#define LOAD_FNX1_COMPUTATION_A_CNT         1
+/**
+ * @brief Number of computation type B done by load function 1
+ * 
+ */
+#define LOAD_FNX1_COMPUTATION_B_CNT         4
+
+/**
+ * @brief Number of peripheral register read done by load function 2
+ * 
+ * the actual number of reads are, this number, times 256
+ * 
+ */
+#define LOAD_FNX2_READ_REGISTER_CNT         2
+/**
+ * @brief Number of peripheral register writes done by load function 2
+ * 
+ */
+#define LOAD_FNX2_WRITE_REGISTER_CNT        1
+/**
+ * @brief Number of computation type A done by load function 2
+ * 
+ */
+#define LOAD_FNX2_COMPUTATION_A_CNT         1
+/**
+ * @brief Number of computation type B done by load function 2
+ * 
+ */
+#define LOAD_FNX2_COMPUTATION_B_CNT         1
+/**
+ * @brief Number of peripheral register read done by load function 3
+ * 
+ * the actual number of reads are, this number, times 256
+ * 
+ */
+#define LOAD_FNX3_READ_REGISTER_CNT         1
+/**
+ * @brief Number of peripheral register writes done by load function 3
+ * 
+ */
+#define LOAD_FNX3_WRITE_REGISTER_CNT        1
+/**
+ * @brief Number of computation type A done by load function 3
+ * 
+ */
+#define LOAD_FNX3_COMPUTATION_A_CNT         1
+/**
+ * @brief Number of computation type B done by load function 3
+ * 
+ */
+#define LOAD_FNX3_COMPUTATION_B_CNT         0
+/**
+ * @brief Number of peripheral register read done by load function 4
+ * 
+ * the actual number of reads are, this number, times 256
+ * 
+ */
+#define LOAD_FNX4_READ_REGISTER_CNT         0
+/**
+ * @brief Number of peripheral register writes done by load function 4
+ * 
+ */
+#define LOAD_FNX4_WRITE_REGISTER_CNT        1
+/**
+ * @brief Number of computation type A done by load function 4
+ * 
+ */
+#define LOAD_FNX4_COMPUTATION_A_CNT         1
+/**
+ * @brief Number of computation type B done by load function 4
+ * 
+ */
+#define LOAD_FNX4_COMPUTATION_B_CNT         1
+
+/**
+ * @brief Linker symbol for start of FLC overlay region in SRAM
+ * 
+ */
+extern char FLC_OVL_RGN_END;
+
+/**
+ * @brief Linker symbol for end of FLC overlay region in SRAM
+ * 
+ */
+extern char FLC_OVL_RGN_START;
+
+/**
+ * @brief Function pointer typedef for the testing function 
+ * 
+ */
 typedef void (*pProfFnx_t) (void);
 
-static inline void ovlAccn(uint32_t function_address)
+/**
+ * @brief Type 1 Load function that will be executed from flash 
+ * 
+ */
+void load_function_t1_xip(void);
+
+/**
+ * @brief Type 1 Load function that will be executed from SRAM 
+ * 
+ */
+void load_function_t1_sram(void);
+
+/**
+ * @brief Type 2 Load function that will be executed from flash 
+ * 
+ */
+void load_function_t2_sram(void);
+
+/**
+ * @brief Type 2 Load function that will be executed from SRAM 
+ * 
+ */
+void load_function_t2_xip(void);
+
+/**
+ * @brief Type 3 Load function that will be executed from flash 
+ * 
+ */
+void load_function_t3_sram(void);
+
+/**
+ * @brief Type 3 Load function that will be executed from SRAM 
+ * 
+ */
+void load_function_t3_xip(void);
+
+/**
+ * @brief Type 4 Load function that will be executed from flash 
+ * 
+ */
+void load_function_t4_sram(void);
+
+/**
+ * @brief Type 4 Load function that will be executed from SRAM 
+ * 
+ */
+void load_function_t4_xip(void);
+
+
+void ovlAccn(uint32_t function_address)
 {
+    uint32_t flc_region_size = (uint32_t)&FLC_OVL_RGN_END - (uint32_t)&FLC_OVL_RGN_START;
+
+    DebugP_assert(flc_region_size >= APP_MINIMUM_FLC_REGION_SIZE);
+
     FLC_RegionInfo region;
     region.baseAddress = CSL_RL2_REGS_R5SS0_CORE0_U_BASE;
-    region.destinationStartAddress = 0x701D8000;
+    region.destinationStartAddress = (uint32_t)&FLC_OVL_RGN_START;
     region.regionId = 0;
     region.sourceStartAddress = function_address;
-    region.sourceEndAddress = function_address + 32*1024;
+    region.sourceEndAddress = function_address + flc_region_size;
 
     FLC_configureRegion(&region);
+
+    /*
+        Increase the transaction buffer size of FLC IP so that CPU can send request, 
+        back to back, to achieve maximum read throughput. This is a workaround and its 
+        value should be set to 0 back after after its being used or if RL2 is being used.
+    */
+    *((volatile uint32_t*)(CSL_RL2_REGS_R5SS0_CORE0_U_BASE + 0x104)) |= 4<<24;
+
     FLC_startRegion(&region);
 }
 
 static inline void profile_fnx( pProfFnx_t pFnx, const char tag[], int ovlEn)
 {
-    // initial measurement
     volatile uint32_t cyc = CSL_armR5PmuReadCntr(CSL_ARM_R5_PMU_CYCLE_COUNTER_NUM);
     volatile uint32_t ix = CSL_armR5PmuReadCntr(0);
     volatile uint32_t icm = CSL_armR5PmuReadCntr(1);
     volatile uint32_t ica = CSL_armR5PmuReadCntr(2);
 
-    // test load
     CacheP_wbInvAll(CacheP_TYPE_ALL);
     if(ovlEn)
     {
@@ -85,7 +250,10 @@ static inline void profile_fnx( pProfFnx_t pFnx, const char tag[], int ovlEn)
     icm = CSL_armR5PmuReadCntr(1) - icm;
     ica = CSL_armR5PmuReadCntr(2) - ica;
 
-    DebugP_log("%s: %d, %d, %d, %d\r\n",tag, cyc , ix  , icm , ica);
+    /* Cycles Per Instruction: here the average value is being calculated */
+    float cpi = (float)cyc / (float)ix;
+
+    DebugP_log("%-15s:%-15d%-15d%3.1f \r\n",tag, cyc , ix  , cpi);
 }
 
 void sram_overlay_main(void *args)
@@ -96,21 +264,28 @@ void sram_overlay_main(void *args)
 
     config_pmu();
 
+    DebugP_log("\r\nTest started\r\n");
+
+    DebugP_log("%-15s:%-15s%-15s%s\r\n", "Label", "CPU Cycles", "CPU Instr.", "CPI");
+
     profile_fnx(load_function_t1_sram,  "T1 SRAM", FALSE);
     profile_fnx(load_function_t1_xip,   "T1 XIP ", FALSE);
     profile_fnx(load_function_t1_xip,   "T1 OVL ", TRUE);
+    DebugP_log("\r\n");
 
     profile_fnx(load_function_t2_sram,  "T2 SRAM", FALSE);
     profile_fnx(load_function_t2_xip,   "T2 XIP ", FALSE);
-    profile_fnx(load_function_t2_xip,   "T2 XIP ", TRUE);
+    profile_fnx(load_function_t2_xip,   "T2 OVL ", TRUE);
+    DebugP_log("\r\n");
 
     profile_fnx(load_function_t3_sram,  "T3 SRAM", FALSE);
     profile_fnx(load_function_t3_xip,   "T3 XIP ", FALSE);
-    profile_fnx(load_function_t3_xip,   "T3 XIP ", TRUE);
+    profile_fnx(load_function_t3_xip,   "T3 OVL ", TRUE);
+    DebugP_log("\r\n");
 
     profile_fnx(load_function_t4_sram,  "T4 SRAM", FALSE);
     profile_fnx(load_function_t4_xip,   "T4 XIP ", FALSE);
-    profile_fnx(load_function_t4_xip,   "T4 XIP ", TRUE);
+    profile_fnx(load_function_t4_xip,   "T4 OVL ", TRUE);
 
     DebugP_log("All tests have passed!!\r\n");
 
@@ -120,41 +295,81 @@ void sram_overlay_main(void *args)
 
 void load_function_t1_xip(void)
 {
-    COMP_BLOCK_256(4, 1, 1, 4);
+    COMP_BLOCK_256(
+        LOAD_FNX1_READ_REGISTER_CNT, 
+        LOAD_FNX1_WRITE_REGISTER_CNT, 
+        LOAD_FNX1_COMPUTATION_A_CNT, 
+        LOAD_FNX1_COMPUTATION_B_CNT
+    );
 }
 
 void load_function_t1_sram(void)
 {
-    COMP_BLOCK_256(4, 1, 1, 4);
+    COMP_BLOCK_256(
+        LOAD_FNX1_READ_REGISTER_CNT,
+        LOAD_FNX1_WRITE_REGISTER_CNT, 
+        LOAD_FNX1_COMPUTATION_A_CNT,
+        LOAD_FNX1_COMPUTATION_B_CNT
+    );
 }
 
 void load_function_t2_sram(void)
 {
-    COMP_BLOCK_256(2, 1, 1, 1);
+    COMP_BLOCK_256(
+        LOAD_FNX2_READ_REGISTER_CNT, 
+        LOAD_FNX2_WRITE_REGISTER_CNT, 
+        LOAD_FNX2_COMPUTATION_A_CNT, 
+        LOAD_FNX2_COMPUTATION_B_CNT
+    );
 }
 
 void load_function_t2_xip(void)
 {
-    COMP_BLOCK_256(2, 1, 1, 1);
+    COMP_BLOCK_256(
+        LOAD_FNX2_READ_REGISTER_CNT, 
+        LOAD_FNX2_WRITE_REGISTER_CNT, 
+        LOAD_FNX2_COMPUTATION_A_CNT, 
+        LOAD_FNX2_COMPUTATION_B_CNT
+    );
 }
 
 void load_function_t3_sram(void)
 {
 
-    COMP_BLOCK_256(1, 1, 1, 0);
+    COMP_BLOCK_256(
+        LOAD_FNX3_READ_REGISTER_CNT, 
+        LOAD_FNX3_WRITE_REGISTER_CNT, 
+        LOAD_FNX3_COMPUTATION_A_CNT, 
+        LOAD_FNX3_COMPUTATION_B_CNT
+    );
 }
 
 void load_function_t3_xip(void)
 {
-    COMP_BLOCK_256(1, 1, 1, 0);
+    COMP_BLOCK_256(
+        LOAD_FNX3_READ_REGISTER_CNT, 
+        LOAD_FNX3_WRITE_REGISTER_CNT, 
+        LOAD_FNX3_COMPUTATION_A_CNT, 
+        LOAD_FNX3_COMPUTATION_B_CNT
+    );
 }
 
 void load_function_t4_sram(void)
 {
-    COMP_BLOCK_256(0, 1, 1, 1);
+    COMP_BLOCK_256(
+        LOAD_FNX4_READ_REGISTER_CNT, 
+        LOAD_FNX4_WRITE_REGISTER_CNT, 
+        LOAD_FNX4_COMPUTATION_A_CNT, 
+        LOAD_FNX4_COMPUTATION_B_CNT
+    );
 }
 
 void load_function_t4_xip(void)
-{
-    COMP_BLOCK_256(0, 1, 1, 1);
+{    
+    COMP_BLOCK_256(
+        LOAD_FNX4_READ_REGISTER_CNT, 
+        LOAD_FNX4_WRITE_REGISTER_CNT, 
+        LOAD_FNX4_COMPUTATION_A_CNT, 
+        LOAD_FNX4_COMPUTATION_B_CNT
+    );
 }
