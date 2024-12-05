db28671cb017d3c690bf98c553fd1a091146e7e7
Piyush Sharma <p-sharma3@ti.com>
2024-12-05 13:19:59 +0530

    am243x: MCAN: Add gcc support in mcan test
    Add gcc-armv7 support in mcan/canfd_hld tests.
    
    Fixes: MCUSDK-11915
    
    Signed-off-by: Piyush Sharma <p-sharma3@ti.com>

--- test/drivers/mcan/canfd_hld/.project/project_am243x.js
+++ test/drivers/mcan/canfd_hld/.project/project_am243x.js
@@ -23,6 +23,7 @@ const libdirs_freertos = {
     common: [
         "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
         "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
         "${MCU_PLUS_SDK_PATH}/test/unity/lib",
     ],
 };
@@ -41,6 +42,14 @@ const libs_freertos_r5f = {
         "freertos.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
         "drivers.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
         "unity.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_r5f_gcc = {
+    common: [
+        "freertos.am243x.r5f.gcc-armv7.${ConfigName}.lib",
+        "drivers.am243x.r5f.gcc-armv7.${ConfigName}.lib",
+        "unity.am243x.r5f.gcc-armv7.${ConfigName}.lib",
     ],
 };
 
@@ -63,9 +72,26 @@ const templates_freertos_r5f =
     }
 ];
 
+const templates_freertos_r5f_gcc =
+[
+    {
+        input: ".project/templates/am243x/common/linker_r5f_gcc.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am243x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "test_main",
+        },
+    }
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-evm", os: "freertos"},
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-lp", os: "freertos"},
+    { device: device, cpu: "r5fss0-0", cgt: "gcc-armv7", board: "am243x-evm", os: "freertos"},
+    { device: device, cpu: "r5fss0-0", cgt: "gcc-armv7", board: "am243x-lp", os: "freertos"},
 ];
 
 function getComponentProperty() {
@@ -94,8 +120,16 @@ function getComponentBuildProperty(buildOption) {
         {
             build_property.includes = includes_freertos_r5f;
             build_property.libdirs = libdirs_freertos;
-            build_property.libs = libs_freertos_r5f;
-            build_property.templates = templates_freertos_r5f;
+            if(buildOption.cgt.match(/gcc*/) )
+            {
+                build_property.libs = libs_freertos_r5f_gcc;
+                build_property.templates = templates_freertos_r5f_gcc;
+            }
+            else
+            {
+                build_property.libs = libs_freertos_r5f;
+                build_property.templates = templates_freertos_r5f;
+            }
         }
     }
 
