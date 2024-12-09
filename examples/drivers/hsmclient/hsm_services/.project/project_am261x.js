2692059aaa50d792dc16671c69dca0cf4c4257e9
Aswathi R <a0505177@ti.com>
2024-12-09 20:14:11 +0530

    am261x: infra: enable hsm client examples build
    - enable top level build for hsmclient examples
    - remove som from build options for both examples
    
    Fixes: MCUSDK-13872
    Signed-off-by: Aswathi R <a-r1@ti.com>

---
+++ examples/drivers/hsmclient/hsm_services/.project/project_am261x.js
@@ -0,0 +1,154 @@
+let path = require('path');
+
+let device = "am261x";
+
+const files = {
+    common: [
+        "hsm_client_app.c",
+        "main.c",
+        "hsm_debug_auth.c",
+        "hsm_getVersion.c",
+        "trng_service_app.c",
+        "hsm_firewall_services.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",       /* core_os_combo base */
+        "../../..", /* Example base */
+    ],
+};
+
+const libdirs_nortos = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/nortos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+        "${MCU_PLUS_SDK_PATH}/source/security/lib",
+    ],
+};
+
+const libdirs_freertos = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+        "${MCU_PLUS_SDK_PATH}/source/security/lib",
+    ],
+};
+
+const includes_freertos_r5f = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_ARM_CLANG/ARM_CR5F",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am261x/r5f",
+        "${MCU_PLUS_SDK_PATH}/source/security",
+    ],
+};
+
+
+const libs_nortos_r5f = {
+    common: [
+        "nortos.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "security.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const libs_freertos_r5f = {
+    common: [
+        "freertos.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "security.am261x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const includes = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/security",
+    ],
+};
+
+const syscfgfile = "../example.syscfg";
+
+const templates_nortos_r5f =
+[
+];
+
+const templates_freertos_r5f =
+[
+    {
+        input: ".project/templates/am261x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "hsm_services",
+        },
+    }
+];
+
+const readmeDoxygenPageTag = "EXAMPLES_HSM_SERVICES";
+
+const buildOptionCombos = [
+    // { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am261x-som", os: "nortos"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am261x-lp", os: "nortos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "hsm_services";
+    property.isInternal = false;
+    property.tirexResourceSubClass = [ "example.gettingstarted" ];
+    property.description = "Demonstrate hsm services message"
+    property.buildOptionCombos = buildOptionCombos;
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.libdirs = libdirs_nortos;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
+
+    if(buildOption.cpu.match(/r5f*/)) {
+        if(buildOption.os.match(/freertos*/) )
+        {
+            build_property.includes = includes_freertos_r5f;
+            build_property.libdirs = libdirs_freertos;
+            build_property.libs = libs_freertos_r5f;
+            build_property.templates = templates_freertos_r5f;
+        }
+        else
+        {
+            build_property.libs = libs_nortos_r5f;
+            build_property.templates = templates_nortos_r5f;
+            build_property.includes = includes;
+        }
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
