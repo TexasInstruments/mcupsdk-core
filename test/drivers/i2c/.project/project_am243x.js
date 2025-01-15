1c107f056f1e67dbd79295abeb75cf476b9d48ad
Edouard Haas <edouard.haas@jblopen.com>
2025-01-15 15:49:58 -0500

    am243x: drivers: add threadx build for i2c driver test.
    - add threadx libs, templates and headers to test project file.
    - add syscfg files.
    
    Fixes: SITSW-5614
    
    Signed-off-by: Edouard Haas <edouard.haas@jblopen.com>
    (cherry picked from commit 4dccee382afb913ca48a9361bf53eee2754a2286)

--- test/drivers/i2c/.project/project_am243x.js
+++ test/drivers/i2c/.project/project_am243x.js
@@ -1,5 +1,7 @@
 let path = require('path');
 
+const device_project = require("../../../../.project/device/project_am243x.js");
+
 let device = "am243x";
 
 const files = {
@@ -29,10 +31,28 @@ const libdirs = {
     ],
 };
 
+const libdirs_threadx = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/threadx/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+        "${MCU_PLUS_SDK_PATH}/test/unity/lib",
+    ],
+};
+
 const includes = {
     common: [
         "${MCU_PLUS_SDK_PATH}/test/unity/",
         "${MCU_PLUS_SDK_PATH}/test/drivers/i2c/am243x-evm",
+    ],
+};
+
+const includes_threadx_r5f = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/test/unity/",
+        "${MCU_PLUS_SDK_PATH}/test/drivers/i2c/am243x-evm",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/threadx/threadx_src/common/inc",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/threadx/ports/ti_arm_gcc_clang_cortex_r5/inc",
     ],
 };
 
@@ -60,6 +80,14 @@ const libs_m4f = {
     ],
 };
 
+const libs_threadx_r5f = {
+    common: [
+        "threadx.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "unity.am243x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
 
 const lnkfiles = {
     common: [
@@ -106,6 +134,17 @@ const templates_nortos_m4f =
     }
 ];
 
+const templates_threadx_r5f =
+[
+    {
+        input: ".project/templates/am243x/threadx/main_threadx.c.xdt",
+        output: "../main.c",
+        options: {
+        entryFunction: "test_main",
+        },
+    }
+];
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-evm", os: "nortos"},
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-evm", os: "freertos"},
@@ -117,6 +156,11 @@ const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "gcc-armv7", board: "am243x-lp", os: "freertos"},
 ];
 
+const buildOptionCombos_threadx = [
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-evm", os: "threadx"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am243x-lp", os: "threadx"},
+];
+
 function getComponentProperty() {
     let property = {};
 
@@ -125,7 +169,15 @@ function getComponentProperty() {
     property.name = "test_i2c";
     property.isInternal = true;
     property.skipProjectSpec = true;
-    property.buildOptionCombos = buildOptionCombos;
+    
+    if (device_project.getThreadXEnabled() == true)
+    {
+        property.buildOptionCombos = buildOptionCombos.concat(buildOptionCombos_threadx);
+    }
+    else
+    {
+        property.buildOptionCombos = buildOptionCombos;
+    }
 
     return property;
 }
@@ -135,24 +187,36 @@ function getComponentBuildProperty(buildOption) {
 
     build_property.files = files;
     build_property.filedirs = filedirs;
-    build_property.includes = includes;
-    build_property.libdirs = libdirs;
     build_property.lnkfiles = lnkfiles;
     build_property.syscfgfile = syscfgfile;
 
     if(buildOption.cpu.match(/r5f*/)) {
-        if(buildOption.cgt.match(/gcc*/) )
+        if (buildOption.os.match(/threadx*/)) 
         {
-            build_property.libs = libs_r5f_gcc;
-            build_property.templates = templates_nortos_r5f_gcc;
-        }
-        else
-        {
-            build_property.libs = libs_r5f;
-            build_property.templates = templates_nortos_r5f;
+            build_property.includes = includes_threadx_r5f;
+            build_property.libdirs = libdirs_threadx; 
+            build_property.libs = libs_threadx_r5f;
+            build_property.templates = templates_threadx_r5f;
+        } 
+        else 
+        {    
+            build_property.includes = includes;
+            build_property.libdirs = libdirs;
+            if(buildOption.cgt.match(/gcc*/) )
+            {
+                build_property.libs = libs_r5f_gcc;
+                build_property.templates = templates_nortos_r5f_gcc;
+            }
+            else
+            {
+                build_property.libs = libs_r5f;
+                build_property.templates = templates_nortos_r5f;
+            }
         }
     }
     if(buildOption.cpu.match(/m4f*/)) {
+        build_property.includes = includes;
+        build_property.libdirs = libdirs;
         build_property.libs = libs_m4f;
         build_property.templates = templates_nortos_m4f;
     }
