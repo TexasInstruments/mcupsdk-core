3d34d94bda846c1189e68f133c8b6b9d6caf7967
Aswathi R <a0505177@ti.com>
2025-06-26 15:28:44 +0530

    allsoc: common: c6000 migration
    - update c6000 compiler version to 8.3.13
    
    Fixes: MCUSDK-14734
    Signed-off-by: Aswathi R <a-r1@ti.com>

--- .project/genProjectSpec.js
+++ .project/genProjectSpec.js
@@ -172,7 +172,7 @@ const utils = {
                 toolchainVersion = '7.2'
                 break;
             case 'ti-c6000':
-                toolchainVersion = '8.3.12'
+                toolchainVersion = '8.3.13'
                 break;
             case 'ti-pru-cgt':
                 toolchainVersion = '2.3.3'
