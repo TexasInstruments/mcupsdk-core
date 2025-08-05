ba861a5c3de518d200481fd51b8ac2f8b7e9516c
Aswathi R <a0505177@ti.com>
2025-08-05 15:35:01 +0530

    allsoc: common: migrate to Clang 4.0.3
    - migrate SDK to clang version 4.0.3
    
    Fixes: MCUSDK-14804
    Signed-off-by: Aswathi R <a-r1@ti.com>

--- .project/genProjectSpec.js
+++ .project/genProjectSpec.js
@@ -163,7 +163,7 @@ const utils = {
 
         switch(cgt) {
             case 'ti-arm-clang':
-                toolchainVersion = '4.0.1.LTS'
+                toolchainVersion = '4.0.3.LTS'
                 break;
             case 'gcc-aarch64':
                 toolchainVersion = '9.2'
@@ -191,7 +191,7 @@ const utils = {
     },
 
     getTiClangVersionProjectSpec: () => {
-        return "4.0.1";
+        return "4.0.3";
     },
 
     getGCCAarch64NoneVersionProjectSpec: () => {
