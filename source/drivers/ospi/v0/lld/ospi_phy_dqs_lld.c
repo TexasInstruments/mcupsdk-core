d304d8275f4b43ffe5f14f1bcfac1f4c7aad59d7
Nikhil M Jain <n-jain1@ti.com>
2025-09-12 13:43:39 +0530

    am64x/am243x: ospi: Update OSPI_phyTuneGrapher
    The current implementation doesn't include 4 as read delay while
    checking for rxDLL and txDLL delays, update the api to include 4 as read
    delay, increase the array size.
    
    Fixes: SITSW-8136
    
    Signed-off-by: Nikhil M Jain <n-jain1@ti.com>

--- source/drivers/ospi/v0/lld/ospi_phy_dqs_lld.c
+++ source/drivers/ospi/v0/lld/ospi_phy_dqs_lld.c
@@ -697,7 +697,7 @@ void OSPI_lld_phyGetTuningData(uint32_t *tuningData, uint32_t *tuningDataSize)
 
 */
 
-int32_t OSPI_lld_phyTuneGrapher(OSPILLD_Handle hOspi, uint32_t flashOffset, uint8_t arrays[4][128][128])
+int32_t OSPI_lld_phyTuneGrapher(OSPILLD_Handle hOspi, uint32_t flashOffset, uint8_t arrays[5][128][128])
 {
     int32_t status = OSPI_SYSTEM_SUCCESS;
     OSPI_PhyConfig searchPoint;
@@ -711,7 +711,7 @@ int32_t OSPI_lld_phyTuneGrapher(OSPILLD_Handle hOspi, uint32_t flashOffset, uint
     /* Perform the Basic PHY configuration for the OSPI controller */
     OSPI_phyBasicConfig(hOspi);
 
-    for(rdDelay = OSPI_PHY_INIT_RD_DELAY; rdDelay < OSPI_PHY_MAX_RD_DELAY; rdDelay++)
+    for(rdDelay = OSPI_PHY_INIT_RD_DELAY; rdDelay <= OSPI_PHY_MAX_RD_DELAY; rdDelay++)
     {
         for(txDll = 0; txDll < 128; txDll++)
         {
