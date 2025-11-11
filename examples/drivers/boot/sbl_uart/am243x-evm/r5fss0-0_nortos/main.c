9827c1d798cedc0fb1555cb34c7dd2c48c6026c2
harini <harini.c@ti.com>
2025-11-11 15:41:34 +0530

    am243x: SBL: Fix SBL UART buffer size
    - SBL UART application fails if the appimage
    size is greater than 384KB.
    - Increase the size of appimage buffer to
    2MB to support loading of bigger appimages.
    
    Fixes: SITSW-7906
    
    Signed-off-by: harini <harini.c@ti.com>

--- examples/drivers/boot/sbl_uart/am243x-evm/r5fss0-0_nortos/main.c
+++ examples/drivers/boot/sbl_uart/am243x-evm/r5fss0-0_nortos/main.c
@@ -47,7 +47,7 @@
 
 #define BOOTLOADER_UART_CPU_RUN_WAIT_SECONDS (2)
 
-#define BOOTLOADER_APPIMAGE_MAX_FILE_SIZE (0x60000) /* Size of section MSRAM_2 specified in linker.cmd */
+#define BOOTLOADER_APPIMAGE_MAX_FILE_SIZE (0x200000) /* Size of section DDR specified in linker.cmd */
 uint8_t gAppImageBuf[BOOTLOADER_APPIMAGE_MAX_FILE_SIZE] __attribute__((aligned(128), section(".bss.filebuf")));
 
 /* call this API to stop the booting process and spin, do that you can connect
@@ -133,7 +133,7 @@ int main(void)
 
             if(SystemP_SUCCESS == status && fileSize == BOOTLOADER_APPIMAGE_MAX_FILE_SIZE)
             {
-                /* A file larger than 384 KB was sent, and xmodem probably dropped bytes */
+                /* A file larger than 2 MB was sent, and xmodem probably dropped bytes */
                 status = SystemP_FAILURE;
 
                 /* Send response to the script that file size exceeded */
