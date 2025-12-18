2b8c068ae0270e9558bca02c55f9fb6dfbfabdb3
Rohith Kumar V <r-v3@ti.com>
2025-12-18 12:49:37 +0530

    am263x: Add support for PG1.2
    - Add SR12 to support PG1.2
    
    Fixes: MCUSDK-15143
    
    Signed-off-by: Rohith Kumar V <r-v3@ti.com>

--- source/drivers/bootloader/soc/am263x/bootloader_hsmRt_load.c
+++ source/drivers/bootloader/soc/am263x/bootloader_hsmRt_load.c
@@ -55,6 +55,7 @@
 /*                           Macros & Typedefs                                */
 /* ========================================================================== */
 #define AM263x_SR11    (0x00000003u)
+#define AM263x_SR12    (0x00000004u)
 /* ========================================================================== */
 /*                            Global Variables                                */
 /* ========================================================================== */
@@ -78,7 +79,8 @@ void Bootloader_socLoadHsmRtFw(HsmClient_t *gHSMClient, const uint8_t *HsmRtFw, 
         DebugP_logInfo("HSMRT Size in Bytes : %ld \r\n", (uint32_t)hsmRTSize);
     }
     else if((ptrTopCtrlRegs->EFUSE_DEVICE_TYPE == BOOTLOADER_DEVTYPE_HSFS) &&
-			(((ptrTopCtrlRegs->EFUSE1_ROW_15)&0xF) == AM263x_SR11))
+			((((ptrTopCtrlRegs->EFUSE1_ROW_15)&0xF) == AM263x_SR11) ||
+             (((ptrTopCtrlRegs->EFUSE1_ROW_15)&0xF) == AM263x_SR12)))
     {
         DebugP_logInfo("Device Type : HSFS  \r\n");
         retVal = Hsmclient_loadHSMRtFirmware(gHSMClient, (uint8_t*)HsmRtFw);
