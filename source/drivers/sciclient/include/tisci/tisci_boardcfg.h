d02c732a1765d24ba737a09d60a30f602acc265c
Fayaz Akthar <f-akthar@ti.com>
2026-02-10 16:17:14 +0530

    am64x_am243x: sysfw: revert boardcfg header changes
    Revert  changes to tisci_boardcfg.h made in commit
    59f337463a9ce9f74ffe010cbe8ee07aa50d81d3 as corresponding
    .c files are not updated.
    
    This commit fixes the build failure that arises due
    to not updating .c files
    
    Fixes: SITSW-8473
    
    Signed-off-by: Fayaz Akthar <f-akthar@ti.com>

--- source/drivers/sciclient/include/tisci/tisci_boardcfg.h
+++ source/drivers/sciclient/include/tisci/tisci_boardcfg.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2018-2026 Texas Instruments Incorporated
+ *  Copyright (C) 2018-2025 Texas Instruments Incorporated
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
@@ -251,16 +251,13 @@ struct tisci_boardcfg_extended_otp {
  * \param subhdr Magic and size for integrity check
  * \param allowed_hosts ID's of hosts allowed to use DKEK. Use TISCI_HOST_ID_ALL if any host is allowed
  * \param allow_dkek_export_tisci If DKEK can be exported via TISCI interface to hosts. Set to 0x5A to allow.
- * \param fwl_reinit_devgrp_opt 0 = disable the fwl_reinit_devgrp API
- *                           0x5A = enable the fwl_reinit_devgrp API
  * \param rsvd Reserved field for future use. Set to 0.
  */
 struct tisci_boardcfg_dkek {
     struct tisci_boardcfg_substructure_header    subhdr;
     uint8_t                    allowed_hosts[MAX_NUM_DKEK_ALLOWED_HOSTS];
     uint8_t                    allow_dkek_export_tisci;
-    uint8_t                    fwl_reinit_devgrp_opt;
-    uint8_t                    rsvd[2];
+    uint8_t                    rsvd[3];
 } __attribute__((__packed__));
 
 /**
