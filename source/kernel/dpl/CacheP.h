180b7ef2a3d5c74cf8830ef99686683a317e541e
Rajesh Chinnasamy <rajesh.chinnasamy@ti.com>
2026-06-10 13:05:19 +0530

    am26xx: dpl: correct the cache alignment
     - Changed CacheP_CACHELINE_ALIGNMENT 128 to 32 based on code review
    
    Fixes: MCUSDK-15706
    
    Signed-off-by: Rajesh Chinnasamy <rajesh.chinnasamy@ti.com>

--- source/kernel/dpl/CacheP.h
+++ source/kernel/dpl/CacheP.h
@@ -57,7 +57,11 @@ extern "C" {
  * Actual CPU defined cache line can be smaller that this value, this define
  * is a utility macro to keep application portable across different CPU's.
  */
+#if defined (SOC_AM263X) || defined (SOC_AM263PX) || defined (SOC_AM261X)
+#define CacheP_CACHELINE_ALIGNMENT   (32U)
+#else
 #define CacheP_CACHELINE_ALIGNMENT   (128U)
+#endif
 
 /**
  *  \anchor CacheP_Type
