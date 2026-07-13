7f75b333530d81d3fa02e570dd93968ade1cdcdf
BensenBernard <b-bernard@ti.com>
2026-07-13 11:32:53 +0530

    am263x: mmcsd:  Fix the register parsing issue
    - Fix parsing of CSD register and CID register in mmcsd_parse.c.
      The previous implementation was shifting the response registers
      and parsing from the shifted values, which was incorrect.
    
    Fixes: MCUSDK-15221
    
    Signed-off-by: Bensen Bernard <b-bernard@ti.com>

--- source/drivers/mmcsd/v1/lld/internal/mmcsd_parse.c
+++ source/drivers/mmcsd/v1/lld/internal/mmcsd_parse.c
@@ -186,24 +186,17 @@ int32_t MMCSD_parseCIDSd(MMCSD_SdDeviceData *data, uint32_t resp[4])
 
     if((data != NULL) && (resp != NULL))
     {
-        /* Shift the responses left by 1 byte */
-        uint32_t tempResp[4];
-        tempResp[3] = (resp[3] << 8) | (resp[2] >> 24);
-        tempResp[2] = (resp[2] << 8) | (resp[1] >> 24);
-        tempResp[1] = (resp[1] << 8) | (resp[0] >> 24);
-        tempResp[0] = (resp[0] << 8);
-
         /* Manufacturer ID - 8 bits 31:24 */
-        data->manuID = (uint8_t)MMCSD_GET_BITFIELD(tempResp[3], 24U, 31U);
+        data->manuID = (uint8_t)MMCSD_GET_BITFIELD(resp[3], 24U, 31U);
 
         /* Product name */
         data->productName[5] = '\0';
-        data->productName[4] = MMCSD_GET_BITFIELD(tempResp[3], 0U, 7U);
-        (void)memcpy(data->productName, &tempResp[2], 4U);
+        data->productName[4] = MMCSD_GET_BITFIELD(resp[3], 0U, 7U);
+        (void)memcpy(data->productName, &resp[2], 4U);
 
         /* Manufacturing date */
-        uint32_t monthCode = MMCSD_GET_BITFIELD(tempResp[0], 8U, 11U);
-        uint16_t yearCode  = (uint16_t)MMCSD_GET_BITFIELD(tempResp[0], 12U, 19U);
+        uint32_t monthCode = MMCSD_GET_BITFIELD(resp[0], 8U, 11U);
+        uint16_t yearCode  = (uint16_t)MMCSD_GET_BITFIELD(resp[0], 12U, 19U);
 
         monthCode = ((monthCode > 11U) || (monthCode < 1U)) ? 1U : monthCode;
 
@@ -224,38 +217,31 @@ int32_t MMCSD_parseCSDSd(MMCSD_SdDeviceData *data, uint32_t resp[4])
 
     if((data != NULL) && (resp != NULL))
     {
-        /* Shift the responses left by 1 byte */
-        uint32_t tempResp[4];
-        tempResp[3] = (resp[3] << 8) | (resp[2] >> 24);
-        tempResp[2] = (resp[2] << 8) | (resp[1] >> 24);
-        tempResp[1] = (resp[1] << 8) | (resp[0] >> 24);
-        tempResp[0] = (resp[0] << 8);
+        data->maxWriteBlockLen = ((uint16_t)1U << (uint16_t)(MMCSD_GET_BITFIELD(resp[0], 22U, 25U)));
+        data->maxReadBlockLen  = ((uint16_t)1U << (uint16_t)(MMCSD_GET_BITFIELD(resp[2], 16U, 19U)));
+        data->transferSpeed = (uint8_t)(MMCSD_GET_BITFIELD(resp[3], 0U, 7U));
 
-        data->maxWriteBlockLen = ((uint16_t)1U << (uint16_t)(MMCSD_GET_BITFIELD(tempResp[0], 22U, 25U))) << 8U;
-        data->maxReadBlockLen  = ((uint16_t)1U << (uint16_t)(MMCSD_GET_BITFIELD(tempResp[2], 16U, 19U)));
-        data->transferSpeed = (uint8_t)(MMCSD_GET_BITFIELD(tempResp[3], 0U, 7U));
-
-        uint32_t csdVersion = MMCSD_GET_BITFIELD(tempResp[3], 30U, 31U);
+        uint32_t csdVersion = MMCSD_GET_BITFIELD(resp[3], 30U, 31U);
 
         if(csdVersion == 0U)
         {
             /* CSD Ver 1.0 Standard Capacity */
             /* Calculate block count of user data area */
-            uint32_t cSize = ((uint32_t)(MMCSD_GET_BITFIELD(tempResp[2], 0U, 9U)) << 2U) | (uint32_t)(MMCSD_GET_BITFIELD(tempResp[1], 30U, 31U));
-            uint32_t mult = ((uint32_t)1U << ((uint32_t)2U + (uint32_t)(MMCSD_GET_BITFIELD(tempResp[1], 15U, 17U))));
+            uint32_t cSize = ((uint32_t)(MMCSD_GET_BITFIELD(resp[2], 0U, 9U)) << 2U) | (uint32_t)(MMCSD_GET_BITFIELD(resp[1], 30U, 31U));
+            uint32_t mult = ((uint32_t)1U << ((uint32_t)2U + (uint32_t)(MMCSD_GET_BITFIELD(resp[1], 15U, 17U))));
             data->blockCount = (cSize + 1U) * mult;
         }
         else if(csdVersion == 1U)
         {
             /* CSD Ver 2.0 High Capacity Extended Capacity */
             /* Calculate block count of user data area */
-            data->blockCount = (uint32_t)1024U * ((uint32_t)1U + (uint32_t)((MMCSD_GET_BITFIELD(tempResp[2], 0U, 5U) << 16U) | MMCSD_GET_BITFIELD(tempResp[1], 16U, 31U)));
+            data->blockCount = (uint32_t)1024U * ((uint32_t)1U + (uint32_t)((MMCSD_GET_BITFIELD(resp[2], 0U, 5U) << 16U) | MMCSD_GET_BITFIELD(resp[1], 16U, 31U)));
         }
         else if(csdVersion == 2U)
         {
             /* CSD Ver 3.0 Ultra High Capacity */
             /* Calculate block count of user data area */
-            data->blockCount = (uint32_t)1024U * ((uint32_t)1U + (uint32_t)((MMCSD_GET_BITFIELD(tempResp[2], 0U, 11U) << 16U) | MMCSD_GET_BITFIELD(tempResp[1], 16U, 31U)));
+            data->blockCount = (uint32_t)1024U * ((uint32_t)1U + (uint32_t)((MMCSD_GET_BITFIELD(resp[2], 0U, 11U) << 16U) | MMCSD_GET_BITFIELD(resp[1], 16U, 31U)));
         }
         else
         {
